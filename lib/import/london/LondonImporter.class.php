36b14b783d7a5bd41897345d92b6af520e6d1865
Rhodri <rhodri@todev10.(none)>
2010-01-27 15:03:12 +0000

    refs #7 working london event import

--- lib/import/london/LondonImporter.class.php
+++ lib/import/london/LondonImporter.class.php
@@ -1,218 +1,101 @@
 <?php
 
 /**
- * Imports data from London database
  *
- * @package projectn.lib.import.london
+ * @author rhodri
  *
- * @author Clarence Lee <clarencelee@timeout.com>
- *
- * @todo confirm address mapping
- * @todo enquire category mapping
- * @todo fax?
- * @todo confirm url for web
- * @todo confirm description
- * @todo confirm TubeExportName
- * @todo confirm TubeStationID
- * @todo enquire opening times
- * @todo enquire ratings
- * @todo enquire provider
  */
 class LondonImporter
 {
-  /**
-   * @var SimpleXMLElement
-   */
-  private $_venueData;
 
-  /**
-   * @var SimpleXMLElement
-   */
-  private $_eventData;
-  
-  /**
-   * @var SimpleXMLElement
-   */
-  private $_venueCategoryInformationData;
+	/**
+	 * @var Vendor
+	 */
+	private $_vendor;
 
-  /**
-   * @var Vendor
-   */
-  private $_vendor;
+	public function __construct( )
+	{
+		$this->_vendor = Doctrine::getTable( 'Vendor' )->getVendorByCityAndLanguage( 'london', 'en-GB' );
+	}
 
-  /**
-   * @var PoiCategory
-   */
-  private $_defaultPoiCategory;
+	public function run()
+	{
+        $this->process( );
+	}
 
-  /**
-   * var geoEncode
-   */
-  private $_geoEncode;
-  
-  /**
-   * @param string Path to Yaml config file
-   */
-  public function  __construct( PoiCategory $defaultCategory, geoEncode $geoEncode )
-  {
-    $this->_vendor = Doctrine::getTable( 'Vendor' )->getVendorByCityAndLanguage( 'london', 'en-GB' );
-    $this->_defaultPoiCategory = $defaultCategory;
-    $this->_geoEncode = $geoEncode;
-  }
-  
-  /**
-   * Runs venue & event processes
-   */
-  public function run( )
-  {
-    $this->processVenues( );
-  }
 
-  /**
-   * Processes the data that has been loadedGB-en
-   *
-   * @param array $data
-   * 
-   */
-  private function processVenues( )
-  {
-    if( !$this->getVenueData() )
-    {
-      throw new ImportException('No venue data set.');
-    }
 
-    if( !$this->getVenueCategoryInformationData() )
-    {
-      throw new ImportException('No venue category information data set.');
-    }
+	private function process()
+	{
+		$currentPage = 1;
+		$resultsPerPage = 1000;
 
-    foreach( $this->_venueData->Venues->Venue as $venue )
-    {
-      $poi = new Poi( );
+        $query = Doctrine_Query::create()
+	                           ->select( 'o.*, v.*, e.*' )
+	                           ->from( 'SLLOccurrence o' )
+	                           ->leftJoin( 'o.SLLVenue v' )
+	                           ->leftJoin( 'o.SLLEvent e' );
+        do
+        {
+            $pager = new Doctrine_Pager( $query, $currentPage, $resultsPerPage );
 
-      $poi[ 'review_date' ] = $venue->ModifiedDate;
+            $items = $pager->execute( );
 
-      $poi[ 'vendor_poi_id' ] = 1;
-      $poi[ 'local_language' ] = 'en-GB';
+            foreach ( $items as $item )
+            {
+	            // insert venue
+	            $poi = new Poi( );
 
-      $poi[ 'poi_name' ] = (string) $venue->Name;
-      $poi[ 'house_no' ] = $venue->BuildingNo;
-      $poi[ 'street' ] = (string) $venue->Address;
-      $poi[ 'city' ] = (string) $venue->City;
-      $poi[ 'district' ] = '';
+	            $poi[ 'Vendor' ] = $this->_vendor;
 
-      $poi[ 'country' ] = 'GBR';
+	            $poi[ 'vendor_poi_id' ] = $item[ 'venue_id' ];
 
-      $poi[ 'additional_address_details' ] = Importer::concatNonBlankStrings(', ', array(
-        $venue->Address1,
-        $venue->Address2,
-        $venue->Address3,
-        $venue->Address4
-      ));
+				$poi[ 'poi_name' ] = $item[ 'SLLVenue' ][ 'name' ];
+				$poi[ 'house_no' ] = $item[ 'SLLVenue' ][ 'building_name' ];
+				$poi[ 'street' ]   = $item[ 'SLLVenue' ][ 'address' ];
+				$poi[ 'city' ]     = 'London';
+				$poi[ 'zips' ]     = $item[ 'SLLVenue' ][ 'postcode' ];
 
-      $postCode = $venue->PostCode;
-      $poi[ 'zips' ] = $postCode;
+				$poi[ 'country' ] = 'GBR';
+                $poi[ 'country_code' ] = (string) 'GB';
+                $poi[ 'local_language' ] = 'en-GB';
 
-      $poi[ 'country_code' ] = (string) 'GB';
-      $poi[ 'extension' ] = '';
+                $poi[ 'latitude' ]  = $item[ 'SLLVenue' ][ 'latitude' ];
+                $poi[ 'longitude' ] = $item[ 'SLLVenue' ][ 'longitude' ];
 
-      $this->_geoEncode->setAddress( $postCode );
-      $this->_geoEncode->getGeoCode( );
-      $poi[ 'latitude' ] = $this->_geoEncode->getLatitude();
-      $poi[ 'longitude' ] = $this->_geoEncode->getLongitude();
+	            $poi->save( );
 
-      $poi[ 'email' ] = $venue->GenEmail;
 
-      $poi[ 'url' ] = $venue->URL; //or URLForWeb?
+                // insert event
+	            $event = new Event( );
 
-      $poi[ 'phone' ] = $venue->Phone;
+	            $event[ 'Vendor' ] = $this->_vendor;
 
-      $poi[ 'phone2' ] = '';
+	            $event[ 'vendor_event_id' ] = $item[ 'event_id' ];
 
-      $poi[ 'fax' ] = ''; //?
+	            $event[ 'name' ] = $item[ 'SLLEvent' ][ 'title' ];
 
-      $poi[ 'vendor_category' ] = '';//?
+                $event->save( );
 
-      $poi[ 'keywords' ] = '';
 
-      $poi[ 'short_description' ] = '';//?
-      $poi[ 'description' ] = '';//?
+                // insert occurrence
+                $occurrence = new EventOccurrence( );
 
-      $poi[ 'public_transport_links' ] = Importer::concatNonBlankStrings(', ', array(
-        $venue->BusInfo,
-        $venue->TubeInfo,
-        $venue->TubeStationID,
-        $venue->RailInfo
-      ));
+                $occurrence[ 'vendor_event_occurrence_id' ] = $item[ 'id' ];
 
-      $cinemaPriceInfo = $venue->CinemaPriceInfo;
-      $musicPriceInfo = $venue->MusicPriceInfo;
+                $occurrence[ 'Event' ] = $event;
+                $occurrence[ 'Poi' ] = $poi;
 
-      $poi[ 'price_information' ] = Importer::concatNonBlankStrings( ', ', array(
-        $venue->CinemaPriceInfo,
-        $venue->MusicPriceInfo
-      ));
+                $occurrence[ 'start' ] = $item[ 'date_start' ];
+                $occurrence[ 'utc_offset' ] = -1;
 
-      $placeId = (string) $venue->PlaceID;
+                $occurrence->save( );
+            }
 
-      $relatedCatInfo = $this->_venueCategoryInformationData
-        ->xpath( '/Exchange/VenueSectionLinks/VenueSectionLink[PlaceID=' . $placeId . ']' );
-      $relatedCatInfo = $relatedCatInfo[ 0 ];
+            $currentPage++;
 
-      $poi[ 'openingtimes' ] = (string) $relatedCatInfo->TimesExport;
-      $poi[ 'star_rating' ] = '';//?
-      $poi[ 'rating' ] = '';//?
+        } while ( $pager->getLastPage( ) >= $currentPage );
 
-      $poi[ 'provider' ] = '';
-
-      $poiCategories = new Doctrine_Collection( Doctrine::getTable( 'PoiCategory' )  );
-      $poiCategories[] = $this->_defaultPoiCategory;
-      $poi[ 'PoiCategories' ] = $poiCategories;
-
-      $poi[ 'Vendor' ] = $this->_vendor;
-
-      $poi->save( );
-    }
-  }
-
-  /**
-   * Processes the data that has been loaded
-   *
-   * @param array $data
-   */
-  private function processEvents( )
-  {
-  }
-
-  public function setVenueCategoryInformationData( SimpleXMLElement $venueCategoryData )
-  {
-    $this->_venueCategoryInformationData = $venueCategoryData;
-  }
-
-  public function getVenueCategoryInformationData( )
-  {
-    return $this->_venueCategoryInformationData;
-  }
-
-  public function getVenueData(  )
-  {
-    return $this->_venueData;
-  }
-
-  public function setVenueData( SimpleXMLElement $venueData )
-  {
-    $this->_venueData = $venueData;
-  }
-
-  public function getEventData( )
-  {
-    return $this->_eventData;
-  }
-
-  public function setEventData( SimpleXMLElement $eventData )
-  {
-    $this->_eventData = $eventData;
-  }
-
+	}
 
 }
