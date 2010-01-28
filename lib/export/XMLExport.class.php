635e1cf9cd5bc4b20d9fcbf9f47d3dd6987d41e0
timmy <timbowler@timeout.com>
2010-01-28 15:44:29 +0000

    added phpdoc tags to classes

--- lib/export/XMLExport.class.php
+++ lib/export/XMLExport.class.php
@@ -1,13 +1,13 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of XMLExportclass
  *
- * @author ralph
+ * @package projectn
+ * @subpackage export.lib
+ *
+ * @author Ralph Schwaninger <ralphschwaninger@timeout.com>
+ * @copyright Timeout Communications Ltd 2009
+ *
+ *
  */
 abstract class XMLExport
 {
