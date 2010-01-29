4491349d8b4d72b7996694cad4a34ce805c94626
Rhodri <rhodri@todev10.(none)>
2010-01-29 00:29:55 +0000

    ref #7 now updates exisiting records if there is a change

--- lib/import/london/LondonImporter.class.php
+++ lib/import/london/LondonImporter.class.php
@@ -54,76 +54,72 @@ class LondonImporter
 			{
 				$poi = Doctrine::getTable( 'Poi' )->findOneByVendorPoiId( $item[ 'venue_id' ] );
 
-				if ( $poi === false )
-				{
-					// insert venue
-					$poi = new Poi( );
+				if ( $poi === false ) $poi = new Poi( );
 
-					$poi[ 'Vendor' ] = $this->_vendor;
 
-					$poi[ 'vendor_poi_id' ] = $item[ 'venue_id' ];
+				$poi[ 'Vendor' ] = $this->_vendor;
 
-					$poi[ 'poi_name' ] = $item[ 'SLLVenue' ][ 'name' ];
-					$poi[ 'house_no' ] = $item[ 'SLLVenue' ][ 'building_name' ];
-					$poi[ 'street' ] = $item[ 'SLLVenue' ][ 'address' ];
-					$poi[ 'city' ] = 'London';
-					$poi[ 'zips' ] = $item[ 'SLLVenue' ][ 'postcode' ];
+				$poi[ 'vendor_poi_id' ] = $item[ 'venue_id' ];
 
-					$poi[ 'country' ] = 'GBR';
-					$poi[ 'country_code' ] = ( string ) 'GB';
-					$poi[ 'local_language' ] = 'en-GB';
+				$poi[ 'poi_name' ] = $item[ 'SLLVenue' ][ 'name' ];
+				$poi[ 'house_no' ] = $item[ 'SLLVenue' ][ 'building_name' ];
+				$poi[ 'street' ] = $item[ 'SLLVenue' ][ 'address' ];
+				$poi[ 'city' ] = 'London';
+				$poi[ 'zips' ] = $item[ 'SLLVenue' ][ 'postcode' ];
 
-					$poi[ 'latitude' ] = $item[ 'SLLVenue' ][ 'latitude' ];
-					$poi[ 'longitude' ] = $item[ 'SLLVenue' ][ 'longitude' ];
+				$poi[ 'country' ] = 'GBR';
+				$poi[ 'country_code' ] = ( string ) 'GB';
+				$poi[ 'local_language' ] = 'en-GB';
 
-					$poi[ 'email' ] = $item[ 'SLLVenue' ][ 'email' ];
-					$poi[ 'url' ] = $item[ 'SLLVenue' ][ 'url' ];
-					$poi[ 'phone' ] = $item[ 'SLLVenue' ][ 'phone' ];
-					$poi[ 'public_transport_links' ] = $item[ 'SLLVenue' ][ 'travel' ];
-					$poi[ 'openingtimes' ] = $item[ 'SLLVenue' ][ 'opening_times' ];
+				$poi[ 'latitude' ] = $item[ 'SLLVenue' ][ 'latitude' ];
+				$poi[ 'longitude' ] = $item[ 'SLLVenue' ][ 'longitude' ];
 
-					$poi->save( );
-				}
+				$poi[ 'email' ] = $item[ 'SLLVenue' ][ 'email' ];
+				$poi[ 'url' ] = $item[ 'SLLVenue' ][ 'url' ];
+				$poi[ 'phone' ] = $item[ 'SLLVenue' ][ 'phone' ];
+				$poi[ 'public_transport_links' ] = $item[ 'SLLVenue' ][ 'travel' ];
+				$poi[ 'openingtimes' ] = $item[ 'SLLVenue' ][ 'opening_times' ];
+
+				$poi->save( );
 
 
 				// insert event
 				$event = Doctrine::getTable( 'Event' )->findOneByVendorEventId( $item[ 'event_id' ] );
 
-				if ( $event === false )
-				{
-					$event = new Event( );
+				if ( $event === false ) $event = new Event( );
 
-					$event[ 'Vendor' ] = $this->_vendor;
 
-					$event[ 'vendor_event_id' ] = $item[ 'event_id' ];
+				$event[ 'Vendor' ] = $this->_vendor;
 
-					$event[ 'name' ] = $item[ 'SLLEvent' ][ 'title' ];
+				$event[ 'vendor_event_id' ] = $item[ 'event_id' ];
 
-					$event->save( );
-				}
+				$event[ 'name' ] = $item[ 'SLLEvent' ][ 'title' ];
+
+				$event->save( );
 
 
 
 				// insert occurrence
 				$occurrence = Doctrine::getTable( 'EventOccurrence' )->find( $item[ 'id' ] );
 
-				if ( $occurrence === false )
-				{
-					$occurrence = new EventOccurrence( );
+				if ( $occurrence === false ) $occurrence = new EventOccurrence( );
 
-					$occurrence[ 'vendor_event_occurrence_id' ] = $item[ 'id' ];
 
-					$occurrence[ 'Event' ] = $event;
-					$occurrence[ 'Poi' ] = $poi;
+				$occurrence[ 'vendor_event_occurrence_id' ] = $item[ 'id' ];
 
-					$occurrence[ 'start' ] = $item[ 'date_start' ];
+				$occurrence[ 'Event' ] = $event;
+				$occurrence[ 'Poi' ] = $poi;
 
-					// calc offset
-					$timeOffset = $zone->getOffset( new DateTime( $item[ 'date_start' ], $zone ) );
-					$occurrence[ 'utc_offset' ] = $timeOffset / 3600;
+				$occurrence[ 'start' ] = $item[ 'date_start' ];
+				$occurrence[ 'end' ]   = $item[ 'date_end' ];
 
-					$occurrence->save( );
-				}
+				// calc offset
+				$timeOffset = $zone->getOffset( new DateTime( $item[ 'date_start' ], $zone ) );
+				$occurrence[ 'utc_offset' ] = $timeOffset / 3600;
+
+				$occurrence->save( );
+
+
 
 				// free memory
 				$poi->free( );
