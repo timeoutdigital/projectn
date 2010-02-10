a14ad0f732069409b929d329f2c8f151b4a112de
Clarence Lee <clarencelee@timeout.com>
2010-02-10 17:58:08 +0000

    refs #9 put try catch around dataMapping

--- lib/import/Importer.class.php
+++ lib/import/Importer.class.php
@@ -81,12 +81,20 @@ class Importer
   public function run()
   {
     $this->output( 'run ' );
-    foreach( $this->getDataMappers() as $dataSource )
+
+    try
     {
-      foreach( $dataSource->getMapMethods() as $mapMethod )
+      foreach( $this->getDataMappers() as $dataSource )
       {
-         $mapMethod->invoke( $dataSource );
+        foreach( $dataSource->getMapMethods() as $mapMethod )
+        {
+           $mapMethod->invoke( $dataSource );
+        }
       }
+    }
+    catch( Exception $e )
+    {
+      echo $e;
     }
   }
 
