71e6ddc90f9406b61cb72857d5297fcc2e5688c7
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-02-12 18:37:55 +0000

    merge

--- lib/model/doctrine/base/BaseVendor.class.php
+++ lib/model/doctrine/base/BaseVendor.class.php
@@ -9,7 +9,7 @@ Doctrine_Manager::getInstance()->bindComponent('Vendor', 'project_n');
  * 
  * @property string $city
  * @property string $language
- * @property string $inertnational_dial_code
+ * @property string $inernational_dial_code
  * @property Doctrine_Collection $Poi
  * @property Doctrine_Collection $VendorPoiCategory
  * @property Doctrine_Collection $Event
@@ -18,26 +18,26 @@ Doctrine_Manager::getInstance()->bindComponent('Vendor', 'project_n');
  * @property Doctrine_Collection $User
  * @property Doctrine_Collection $ImportStats
  * 
- * @method string              getCity()                    Returns the current record's "city" value
- * @method string              getLanguage()                Returns the current record's "language" value
- * @method string              getInertnationalDialCode()   Returns the current record's "inertnational_dial_code" value
- * @method Doctrine_Collection getPoi()                     Returns the current record's "Poi" collection
- * @method Doctrine_Collection getVendorPoiCategory()       Returns the current record's "VendorPoiCategory" collection
- * @method Doctrine_Collection getEvent()                   Returns the current record's "Event" collection
- * @method Doctrine_Collection getVendorEventCategory()     Returns the current record's "VendorEventCategory" collection
- * @method Doctrine_Collection getMovie()                   Returns the current record's "Movie" collection
- * @method Doctrine_Collection getUser()                    Returns the current record's "User" collection
- * @method Doctrine_Collection getImportStats()             Returns the current record's "ImportStats" collection
- * @method Vendor              setCity()                    Sets the current record's "city" value
- * @method Vendor              setLanguage()                Sets the current record's "language" value
- * @method Vendor              setInertnationalDialCode()   Sets the current record's "inertnational_dial_code" value
- * @method Vendor              setPoi()                     Sets the current record's "Poi" collection
- * @method Vendor              setVendorPoiCategory()       Sets the current record's "VendorPoiCategory" collection
- * @method Vendor              setEvent()                   Sets the current record's "Event" collection
- * @method Vendor              setVendorEventCategory()     Sets the current record's "VendorEventCategory" collection
- * @method Vendor              setMovie()                   Sets the current record's "Movie" collection
- * @method Vendor              setUser()                    Sets the current record's "User" collection
- * @method Vendor              setImportStats()             Sets the current record's "ImportStats" collection
+ * @method string              getCity()                   Returns the current record's "city" value
+ * @method string              getLanguage()               Returns the current record's "language" value
+ * @method string              getInernationalDialCode()   Returns the current record's "inernational_dial_code" value
+ * @method Doctrine_Collection getPoi()                    Returns the current record's "Poi" collection
+ * @method Doctrine_Collection getVendorPoiCategory()      Returns the current record's "VendorPoiCategory" collection
+ * @method Doctrine_Collection getEvent()                  Returns the current record's "Event" collection
+ * @method Doctrine_Collection getVendorEventCategory()    Returns the current record's "VendorEventCategory" collection
+ * @method Doctrine_Collection getMovie()                  Returns the current record's "Movie" collection
+ * @method Doctrine_Collection getUser()                   Returns the current record's "User" collection
+ * @method Doctrine_Collection getImportStats()            Returns the current record's "ImportStats" collection
+ * @method Vendor              setCity()                   Sets the current record's "city" value
+ * @method Vendor              setLanguage()               Sets the current record's "language" value
+ * @method Vendor              setInernationalDialCode()   Sets the current record's "inernational_dial_code" value
+ * @method Vendor              setPoi()                    Sets the current record's "Poi" collection
+ * @method Vendor              setVendorPoiCategory()      Sets the current record's "VendorPoiCategory" collection
+ * @method Vendor              setEvent()                  Sets the current record's "Event" collection
+ * @method Vendor              setVendorEventCategory()    Sets the current record's "VendorEventCategory" collection
+ * @method Vendor              setMovie()                  Sets the current record's "Movie" collection
+ * @method Vendor              setUser()                   Sets the current record's "User" collection
+ * @method Vendor              setImportStats()            Sets the current record's "ImportStats" collection
  * 
  * @package    sf_sandbox
  * @subpackage model
@@ -59,7 +59,7 @@ abstract class BaseVendor extends sfDoctrineRecord
              'notnull' => true,
              'length' => '10',
              ));
-        $this->hasColumn('inertnational_dial_code', 'string', 3, array(
+        $this->hasColumn('inernational_dial_code', 'string', 3, array(
              'type' => 'string',
              'length' => '3',
              ));
