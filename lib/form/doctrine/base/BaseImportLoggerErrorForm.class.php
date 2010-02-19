21ad1052cc6cf9d0fcc80645eea8f10613821773
Clarence Lee <clarencelee@timeout.com>
2010-02-19 14:16:03 +0000

    add generated files

--- lib/form/doctrine/base/BaseImportLoggerErrorForm.class.php
+++ lib/form/doctrine/base/BaseImportLoggerErrorForm.class.php
@@ -15,27 +15,29 @@ abstract class BaseImportLoggerErrorForm extends BaseFormDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'id'               => new sfWidgetFormInputHidden(),
-      'trace'            => new sfWidgetFormTextarea(),
-      'log'              => new sfWidgetFormTextarea(),
-      'type'             => new sfWidgetFormTextarea(),
-      'message'          => new sfWidgetFormTextarea(),
-      'import_logger_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('ImportLogger'), 'add_empty' => false)),
-      'resolved'         => new sfWidgetFormInputCheckbox(),
-      'created_at'       => new sfWidgetFormDateTime(),
-      'updated_at'       => new sfWidgetFormDateTime(),
+      'id'                => new sfWidgetFormInputHidden(),
+      'trace'             => new sfWidgetFormTextarea(),
+      'log'               => new sfWidgetFormTextarea(),
+      'type'              => new sfWidgetFormTextarea(),
+      'message'           => new sfWidgetFormTextarea(),
+      'serialized_object' => new sfWidgetFormTextarea(),
+      'import_logger_id'  => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('ImportLogger'), 'add_empty' => false)),
+      'resolved'          => new sfWidgetFormInputCheckbox(),
+      'created_at'        => new sfWidgetFormDateTime(),
+      'updated_at'        => new sfWidgetFormDateTime(),
     ));
 
     $this->setValidators(array(
-      'id'               => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
-      'trace'            => new sfValidatorString(),
-      'log'              => new sfValidatorString(),
-      'type'             => new sfValidatorString(),
-      'message'          => new sfValidatorString(),
-      'import_logger_id' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('ImportLogger'))),
-      'resolved'         => new sfValidatorBoolean(array('required' => false)),
-      'created_at'       => new sfValidatorDateTime(),
-      'updated_at'       => new sfValidatorDateTime(),
+      'id'                => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
+      'trace'             => new sfValidatorString(),
+      'log'               => new sfValidatorString(),
+      'type'              => new sfValidatorString(),
+      'message'           => new sfValidatorString(),
+      'serialized_object' => new sfValidatorString(),
+      'import_logger_id'  => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('ImportLogger'))),
+      'resolved'          => new sfValidatorBoolean(array('required' => false)),
+      'created_at'        => new sfValidatorDateTime(),
+      'updated_at'        => new sfValidatorDateTime(),
     ));
 
     $this->widgetSchema->setNameFormat('import_logger_error[%s]');
