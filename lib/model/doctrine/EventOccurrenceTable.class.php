a710f1aafb361496039163cfd33afd835970a101
Clarence Lee <clarencelee@timeout.com>
2010-02-23 12:47:01 +0000

    refs #105 EventOccurrence also needs to implment the 'polymorphic' helping method

--- lib/model/doctrine/EventOccurrenceTable.class.php
+++ lib/model/doctrine/EventOccurrenceTable.class.php
@@ -2,4 +2,17 @@
 
 class EventOccurrenceTable extends Doctrine_Table
 {
+
+
+  /**
+   * Get the name of the vendor's uid fieldname, this is a temporary solution
+   * @todo rename Poi, Events, Movies etc to have vendor_uid field instead
+   * of vendor_<model name>_id to allow polymorphism
+   *
+   * @return string
+   */
+  public function getVendorUidFieldName()
+  {
+    return 'vendor_event_occurrence_id';
+  }
 }
