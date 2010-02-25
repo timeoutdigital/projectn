8816b3f2eceeed581d82f49c6101489c0ff916fd
Clarence Lee <clarencelee@timeout.com>
2010-02-25 14:28:46 +0000

    resolves #122 added html purifier libraray

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -5,10 +5,27 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+  static protected $htmlPurifierLoaded = false;
+  
   public function setup()
   {
     $this->enablePlugins('sfDoctrinePlugin');
     //$this->enablePlugins('tmCsvPlugin');
     $this->enablePlugins('toLondonPlugin');
+  }
+
+  static public function registerHTMLPurifier()
+  {
+    if(self::$htmlPurifierLoaded)
+      return;
+
+    require_once sfConfig::get('sf_lib_dir').'/vendor/htmlpurifier-library/HTMLPurifier/Bootstrap.php';
+    spl_autoload_register(array('HTMLPurifier_Bootstrap', 'autoload'));
+
+    self::$htmlPurifierLoaded = true;
+
+    $config = HTMLPurifier_Config::createDefault();
+    $config->set('Cache.DefinitionImpl', null);
+    $config->set('HTML.Allowed', 'p,b,a[href],i,br,pre');
   }
 }
