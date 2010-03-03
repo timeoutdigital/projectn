bbde122644ea01d4931aa2698caa23379c27cd2e
timmy <timbowler@timeout.com>
2010-03-03 19:04:38 +0000

    refs #131 Added output text for NY and Chicago

--- lib/task/importTask.class.php
+++ lib/task/importTask.class.php
@@ -393,7 +393,7 @@ class importTask extends sfBaseTask
         }
         catch ( Exception $e )
         {
-          echo 'Exception caught in chicago' . $options['city'] . ' ' . $options['type'] . ' import: ' . $e->getMessage();
+          echo 'Exception caught in chicago' . $e->getMessage();
         }
   }
 
