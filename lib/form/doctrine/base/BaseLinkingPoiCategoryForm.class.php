5e648fab362d8e6e5dbcbc5da0183aeda8e55e2a
Clarence Lee <clarencelee@timeout.com>
2010-03-03 17:53:57 +0000

    generated files

--- lib/form/doctrine/base/BaseLinkingPoiCategoryForm.class.php
+++ lib/form/doctrine/base/BaseLinkingPoiCategoryForm.class.php
@@ -1,48 +1,3 @@
-<<<<<<< .merge_file_J4guu0
-<?php
-
-/**
- * LinkingPoiCategory form base class.
- *
- * @method LinkingPoiCategory getObject() Returns the current form's model object
- *
- * @package    sf_sandbox
- * @subpackage form
- * @author     Your name here
- * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
- */
-abstract class BaseLinkingPoiCategoryForm extends BaseFormDoctrine
-{
-  public function setup()
-  {
-    $this->setWidgets(array(
-      'id'              => new sfWidgetFormInputHidden(),
-      'poi_category_id' => new sfWidgetFormInputText(),
-      'poi_id'          => new sfWidgetFormInputText(),
-    ));
-
-    $this->setValidators(array(
-      'id'              => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
-      'poi_category_id' => new sfValidatorInteger(),
-      'poi_id'          => new sfValidatorInteger(),
-    ));
-
-    $this->widgetSchema->setNameFormat('linking_poi_category[%s]');
-
-    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
-
-    $this->setupInheritance();
-
-    parent::setup();
-  }
-
-  public function getModelName()
-  {
-    return 'LinkingPoiCategory';
-  }
-
-}
-=======
 <?php
 
 /**
@@ -62,13 +17,13 @@ abstract class BaseLinkingPoiCategoryForm extends BaseFormDoctrine
     $this->setWidgets(array(
       'id'              => new sfWidgetFormInputHidden(),
       'poi_category_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('PoiCategory'), 'add_empty' => false)),
-      'poi_id'          => new sfWidgetFormInputText(),
+      'poi_id'          => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Poi'), 'add_empty' => false)),
     ));
 
     $this->setValidators(array(
       'id'              => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
       'poi_category_id' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('PoiCategory'))),
-      'poi_id'          => new sfValidatorInteger(),
+      'poi_id'          => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Poi'))),
     ));
 
     $this->widgetSchema->setNameFormat('linking_poi_category[%s]');
@@ -86,4 +41,3 @@ abstract class BaseLinkingPoiCategoryForm extends BaseFormDoctrine
   }
 
 }
->>>>>>> .merge_file_ZfSZiX
