8751f7757f375213c62672877b3fe84ca78cf631
timmy <timbowler@timeout.com>
2010-03-05 14:11:50 +0000

    Added a host option

--- sync.sh
+++ sync.sh
@@ -8,6 +8,9 @@ then
  echo "Please enter your username:"
  read username
 
+ echo "Host"
+ read host
+
  echo "is this a dry run:"
  read run
 
@@ -19,5 +22,5 @@ then
  fi
 fi
 
-rsync -rlhvzCcp   $dry --delete --progress --force --exclude "sync.sh" --exclude "/test/" --exclude "/web/uploads/" --exclude "/doc/" --exclude "/cache/" --exclude ".*" --exclude "/nbproject/" --exclude "/log/" --exclude "/config/"  . $username@192.9.215.2:/var/www/vhosts/projectn/httpdocs
+rsync -rlhvzCcp   $dry --delete --progress --force --exclude "sync.sh" --exclude "/export/" --exclude "/import/" --exclude "/test/" --exclude "/web/uploads/" --exclude "/doc/" --exclude "/cache/" --exclude ".*" --exclude "/nbproject/" --exclude "/log/" --exclude "/config/"  . $username@$host:/var/vhosts/projectn/httpdocs
 
