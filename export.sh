6c2a622c0c7fa00773f47327384048c8ba277908
timmy <timbowler@timeout.com>
2010-03-22 17:53:50 +0000

    Fixed date bug

--- export.sh
+++ export.sh
@@ -8,72 +8,72 @@ touch export/export_$(date +"%Y%m%d")/upload.lock
 
 echo "Exporting Movies"
 echo "Exporting Movies for abu dhabi"
-./symfony projectn:export --type=movie --city="abu dhabi" --language=en-US --destination=export/export_20100322/movies/abudhabi.xml
+./symfony projectn:export --type=movie --city="abu dhabi" --language=en-US --destination=export/export_$(date +"%Y%m%d")/movies/abudhabi.xml
 
 echo "Exporting Movies for Dubai"
-./symfony projectn:export --type=movie --city="dubai" --language=en-US --destination=export/export_20100322/movies/dubai.xml
+./symfony projectn:export --type=movie --city="dubai" --language=en-US --destination=export/export_$(date +"%Y%m%d")/movies/dubai.xml
 
 echo "Exporting Movies for Lisbon"
-./symfony projectn:export --type=movie --city="lisbon" --language=pt --destination=export/export_20100322/movies/lisbon.xml
+./symfony projectn:export --type=movie --city="lisbon" --language=pt --destination=export/export_$(date +"%Y%m%d")/movies/lisbon.xml
 
 echo "Exporting Movies for London"
-./symfony projectn:export --type=movie --city="london" --language=en-GB --destination=export/export_20100322/movies/london.xml
+./symfony projectn:export --type=movie --city="london" --language=en-GB --destination=export/export_$(date +"%Y%m%d")/movies/london.xml
 
 echo "Exporting Movies for Singapore"
-./symfony projectn:export --type=movie --city="singapore" --language=en-US --destination=export/export_20100322/movies/singapore.xml
+./symfony projectn:export --type=movie --city="singapore" --language=en-US --destination=export/export_$(date +"%Y%m%d")/movies/singapore.xml
 
 echo "Exporting Movies for NY"
-./symfony projectn:export --type=movie --city="ny" --language=en-US --destination=export/export_20100322/movies/ny.xml
+./symfony projectn:export --type=movie --city="ny" --language=en-US --destination=export/export_$(date +"%Y%m%d")/movies/ny.xml
 
 echo "Exporting Movies for Chicago"
-./symfony projectn:export --type=movie --city="chicago" --language=en-US --destination=export/export_20100322/movies/chicago.xml
+./symfony projectn:export --type=movie --city="chicago" --language=en-US --destination=export/export_$(date +"%Y%m%d")/movies/chicago.xml
 
 
 echo "========================================================================="
 
 echo "Exporting Pois"
 echo "Exporting Pois for abu dhabi"
-./symfony projectn:export --type=poi --city="abu dhabi" --language=en-US --destination=export/export_20100322/pois/abudhabi.xml
+./symfony projectn:export --type=poi --city="abu dhabi" --language=en-US --destination=export/export_$(date +"%Y%m%d")/pois/abudhabi.xml
 
 echo "Exporting Pois for Dubai"
-./symfony projectn:export --type=poi --city="dubai" --language=en-US --destination=export/export_20100322/pois/dubai.xml
+./symfony projectn:export --type=poi --city="dubai" --language=en-US --destination=export/export_$(date +"%Y%m%d")/pois/dubai.xml
 
 echo "Exporting Pois for Lisbon"
-./symfony projectn:export --type=poi --city="lisbon" --language=pt --destination=export/export_20100322/pois/lisbon.xml
+./symfony projectn:export --type=poi --city="lisbon" --language=pt --destination=export/export_$(date +"%Y%m%d")/pois/lisbon.xml
 
 echo "Exporting Pois for London"
-./symfony projectn:export --type=poi --city="london" --language=en-GB --destination=export/export_20100322/pois/london.xml
+./symfony projectn:export --type=poi --city="london" --language=en-GB --destination=export/export_$(date +"%Y%m%d")/pois/london.xml
 
 echo "Exporting Movies for Singapore"
-./symfony projectn:export --type=poi --city="singapore" --language=en-US --destination=export/export_20100322/pois/singapore.xml
+./symfony projectn:export --type=poi --city="singapore" --language=en-US --destination=export/export_$(date +"%Y%m%d")/pois/singapore.xml
 
 echo "Exporting Pois for NY"
-./symfony projectn:export --type=poi --city="ny" --language=en-US --destination=export/export_20100322/pois/ny.xml
+./symfony projectn:export --type=poi --city="ny" --language=en-US --destination=export/export_$(date +"%Y%m%d")/pois/ny.xml
 
 echo "Exporting Pois for Chicago"
-./symfony projectn:export --type=poi --city="chicago" --language=en-US --destination=export/export_20100322/pois/chicago.xml
+./symfony projectn:export --type=poi --city="chicago" --language=en-US --destination=export/export_$(date +"%Y%m%d")/pois/chicago.xml
 
 
 echo "========================================================================="
 
 echo "Exporting Events"
 echo "Exporting Events for abu dhabi"
-./symfony projectn:export --type=event --city="abu dhabi" --language=en-US --destination=export/export_20100322/events/abudhabi.xml
+./symfony projectn:export --type=event --city="abu dhabi" --language=en-US --destination=export/export_$(date +"%Y%m%d")/events/abudhabi.xml
 
 echo "Exporting Events for Dubai"
-./symfony projectn:export --type=event --city="dubai" --language=en-US --destination=export/export_20100322/events/dubai.xml
+./symfony projectn:export --type=event --city="dubai" --language=en-US --destination=export/export_$(date +"%Y%m%d")/events/dubai.xml
 
 echo "Exporting Events for Lisbon"
-./symfony projectn:export --type=event --city="lisbon" --language=pt --destination=export/export_20100322/events/lisbon.xml
+./symfony projectn:export --type=event --city="lisbon" --language=pt --destination=export/export_$(date +"%Y%m%d")/events/lisbon.xml
 
 echo "Exporting Events for London"
-./symfony projectn:export --type=event --city="london" --language=en-GB --destination=export/export_20100322/events/london.xml
+./symfony projectn:export --type=event --city="london" --language=en-GB --destination=export/export_$(date +"%Y%m%d")/events/london.xml
 
 echo "Exporting Events for Singapore"
-./symfony projectn:export --type=event --city="singapore" --language=en-US --destination=export/export_20100322/events/singapore.xml
+./symfony projectn:export --type=event --city="singapore" --language=en-US --destination=export/export_$(date +"%Y%m%d")/events/singapore.xml
 
 echo "Exporting Events for NY"
-./symfony projectn:export --type=event --city="ny" --language=en-US --destination=export/export_20100322/events/ny.xml
+./symfony projectn:export --type=event --city="ny" --language=en-US --destination=export/export_$(date +"%Y%m%d")/events/ny.xml
 
 echo "Exporting Events for Chicago"
-./symfony projectn:export --type=event --city="chicago" --language=en-US --destination=export/export_20100322/events/chicago.xml
+./symfony projectn:export --type=event --city="chicago" --language=en-US --destination=export/export_$(date +"%Y%m%d")/events/chicago.xml
