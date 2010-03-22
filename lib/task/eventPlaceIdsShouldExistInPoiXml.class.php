9619f59d153ec3da409175308e19b7e8870a1b58
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-03-22 12:06:22 +0000

    refs #213, #214 rename base class for verification tests

--- lib/task/eventPlaceIdsShouldExistInPoiXml.class.php
+++ lib/task/eventPlaceIdsShouldExistInPoiXml.class.php
@@ -11,7 +11,7 @@
  * @version 1.0.1
  *
  */
-class eventPlaceIdsShouldExistInPoiXml extends verifyTask
+class eventPlaceIdsShouldExistInPoiXml extends baseVerifyTask
 {
   protected function verify()
   {
