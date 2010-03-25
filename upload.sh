747afe3f36342b100df3390c440bf18236c9f859
timmy <timbowler@timeout.com>
2010-03-25 17:47:57 +0000

    refs #184 added live server details

--- upload.sh
+++ upload.sh
@@ -14,29 +14,29 @@ md5sum export/export_$(date +"%Y%m%d")/event/event.zip > export/export_$(date +"
 
 
 echo "Uploading lock"
-lftp -c 'open -e "put export/export_'$(date +"%Y%m%d")'/upload.lock" -u timmy,outtime99 dev '
+lftp -c 'open -e "put export/export_'$(date +"%Y%m%d")'/upload.lock" -u timeout,ot1M0T#8 pictis.msudev.noklab.net '
 
 echo " ------------------------------------------------ "
 
 echo -e "Removing remote movies\n"
-lftp -c 'open -e "rm movie/movie.zip" -u timmy,outtime99 dev '
-lftp -c 'open -e "rm movie/movie.zip.md5" -u timmy,outtime99 dev '
+lftp -c 'open -e "rm movie/movie.zip" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
+lftp -c 'open -e "rm movie/movie.zip.md5" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
 
 echo -e "Removing remote pois\n"
-lftp -c 'open -e "rm poi/poi.zip" -u timmy,outtime99 dev '
-lftp -c 'open -e "rm poi/poi.zip.md5" -u timmy,outtime99 dev '
+lftp -c 'open -e "rm poi/poi.zip" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
+lftp -c 'open -e "rm poi/poi.zip.md5" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
 
 
 echo -e "Removing remote events\n"
-lftp -c 'open -e "rm event/event.zip" -u timmy,outtime99 dev '
-lftp -c 'open -e "rm event/event.zip.md5" -u timmy,outtime99 dev '
+lftp -c 'open -e "rm event/event.zip" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
+lftp -c 'open -e "rm event/event.zip.md5" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
 
 echo -e " ------------------------------------------------ \n"
 
 echo -e "Syncing files\n"
-lftp -c 'open -e "mirror -R -x xml /var/workspace/projectn/export/export_'$(date +"%Y%m%d")' ." -u timmy,outtime99 dev '
+lftp -c 'open -e "mirror -R -x xml /var/workspace/projectn/export/export_'$(date +"%Y%m%d")' ." -u timeout,t1M0T#8 pictis.msudev.noklab.net '
 
 echo -e "Removing lock file\n"
-lftp -c 'open -e "rm upload.lock" -u timmy,outtime99 dev '
+lftp -c 'open -e "rm upload.lock" -u timeout,t1M0T#8 pictis.msudev.noklab.net '
 
 echo "Job Done!!"
