39d52fb7f39d851206562e108380078bdf7e4d51
Timmy Bowler <timbowler@timeout.com>
2010-04-09 10:30:28 +0100

    refs #243 created basic poi and movie backend

---
+++ apps/backend/modules/poi/lib/poiGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * poi module configuration.
+ *
+ * @package    sf_sandbox
+ * @subpackage poi
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class poiGeneratorConfiguration extends BasePoiGeneratorConfiguration
+{
+}
---
+++ apps/backend/modules/poi/lib/poiGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * poi module helper.
+ *
+ * @package    sf_sandbox
+ * @subpackage poi
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class poiGeneratorHelper extends BasePoiGeneratorHelper
+{
+}
