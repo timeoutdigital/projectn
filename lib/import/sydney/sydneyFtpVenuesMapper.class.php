6a723ea664c27e7f5a942d514f678874fce78602
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-05-06 22:14:55 +0100

    refs #299 fix date format

--- lib/import/sydney/sydneyFtpVenuesMapper.class.php
+++ lib/import/sydney/sydneyFtpVenuesMapper.class.php
@@ -78,6 +78,12 @@ class sydneyFtpVenuesMapper extends DataMapper
 
   private function extractDate( $dateString )
   {
+    if ( empty( $dateString ) )
+      return;
+
+    // swap 29/03/2010 9:59:00 AM  to   03/29/2010 9:59:00 AM
+    $dateString = preg_replace( '/([0-9]{2})\/([0-9]{2})\/([0-9]{4} [0-9]+\:[0-9]{2}\:[0-9]{2} [AMP]{2})/', '$2/$1/$3', $dateString );
+
     $date = new DateTime( $dateString );
     return $date->format( 'Y-m-d H:i:s' );
   }
