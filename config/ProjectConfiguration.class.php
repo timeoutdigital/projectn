9fa8d2afa5d9897545c0dd1521639c7fce15ec0a
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-06-23 18:28:53 +0100

    refs #424 add sfDoctrineGuardPlugin

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -12,6 +12,7 @@ class ProjectConfiguration extends sfProjectConfiguration
     $this->enablePlugins('sfDoctrinePlugin');
     //$this->enablePlugins('tmCsvPlugin');
     $this->enablePlugins('toLondonPlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
   }
 
   static public function registerHTMLPurifier()
