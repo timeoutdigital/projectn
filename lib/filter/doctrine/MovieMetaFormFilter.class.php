27640e7ac9f01c23294028248f6d1ae2bf132862
Peter Johnson <peterjohnson@timeout.com>
2010-06-30 13:16:32 +0100

    refs: #443 - Added Meta Tables to Fixtures and Created Models

---
+++ lib/filter/doctrine/MovieMetaFormFilter.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * MovieMeta filter form.
+ *
+ * @package    sf_sandbox
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class MovieMetaFormFilter extends BaseMovieMetaFormFilter
+{
+  /**
+   * @see MetaFormFilter
+   */
+  public function configure()
+  {
+    parent::configure();
+  }
+}
