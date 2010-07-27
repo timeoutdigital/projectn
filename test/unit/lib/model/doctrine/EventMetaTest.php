f501e0b82865d257a02deb54bb6064475580371c
Peter Johnson <peterjohnson@timeout.com>
2010-07-27 12:26:49 +0100

    refs #500 - Removed Unused Tests & Classes & Methods.

--- test/unit/lib/model/doctrine/EventMetaTest.php
+++ test/unit/lib/model/doctrine/EventMetaTest.php
@@ -17,7 +17,7 @@ require_once dirname(__FILE__).'/../../../bootstrap.php';
  *
  *
  */
-class MetaTest extends PHPUnit_Framework_TestCase
+class EventMetaTest extends PHPUnit_Framework_TestCase
 {
   /**
    * Sets up the fixture, for example, opens a network connection.
