3c6ab071c51ee0882c84b8e1889039c10ea4c28d
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-07-30 15:12:39 +0100

    resolves #521

--- lib/import/singapore/singaporeImport.class.php
+++ lib/import/singapore/singaporeImport.class.php
@@ -562,16 +562,16 @@ class singaporeImport
 
         foreach( $datesArray as $date )
         {
+            $vendorEventOccurrenceId = Doctrine::getTable( 'EventOccurrence' )->generateVendorEventOccurrenceId( $eventId, $poi[ 'id' ], $date[ 'start' ] );
+            $eventOccurrence = Doctrine::getTable( 'EventOccurrence' )->findOneByVendorEventOccurrenceId( $vendorEventOccurrenceId );
+
+            if ( $eventOccurrence === false )
+            {
+                $eventOccurrence = new EventOccurrence();
+                $eventOccurrence[ 'vendor_event_occurrence_id' ] = $vendorEventOccurrenceId;
+            }
+
             try {
-                $vendorEventOccurrenceId = Doctrine::getTable( 'EventOccurrence' )->generateVendorEventOccurrenceId( $eventId, $poi[ 'id' ], $date[ 'start' ] );
-                $eventOccurrence = Doctrine::getTable( 'EventOccurrence' )->findOneByVendorEventOccurrenceId( $vendorEventOccurrenceId );
-
-                if ( $eventOccurrence === false )
-                {
-                    $eventOccurrence = new EventOccurrence();
-                    $eventOccurrence[ 'vendor_event_occurrence_id' ] = $vendorEventOccurrenceId;
-                }
-
                 //$eventOccurrence[ 'booking_url' ] ='';
                 $eventOccurrence[ 'utc_offset' ] = $this->_vendor->getUtcOffset( $date[ 'start' ] );
 
