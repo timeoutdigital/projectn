13b331ec018446bd3e612067259405b3bc13e1f5
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-08-03 16:45:06 +0100

    refs #512 add some echo output

--- scripts/clean_releases.sh
+++ scripts/clean_releases.sh
@@ -43,6 +43,7 @@ do
     if [[ -d $CONFIG_DEPLOY_PATH/$CONFIG_APP_NAME/releases/$f ]]; then
         if [[ "$COUNTER" -gt "$CONFIG_KEEP_OLD_RELEASES" ]]; then
             rm -rf $CONFIG_DEPLOY_PATH/$CONFIG_APP_NAME/releases/$f
+            echo "delete old release: $CONFIG_DEPLOY_PATH/$CONFIG_APP_NAME/releases/$f"
         fi
     fi
     let "COUNTER = $COUNTER + 1"
