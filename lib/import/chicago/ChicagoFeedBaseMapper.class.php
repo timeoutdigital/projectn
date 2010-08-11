a847815e1f1f06b86d494c97691a77f2157d28ca
Rajeevan <rajeevankumarathasan@timeout.com>
2010-08-11 14:31:01 +0100

    refs #572 - Chicago poi import complete with test and test data...

---
+++ lib/import/chicago/ChicagoFeedBaseMapper.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Chicago Feed Base mapper
+ *
+ * @package projectn
+ * @subpackage
+ *
+ * @author Rajeevan kumarathasan <rajeevankumarathasan@timeout.com>
+ * @copyright Timeout Communications Ltd
+ *
+ * @version 1.0.0
+ *
+ */
+class ChicagoFeedBaseMapper extends DataMapper
+{
+    /**
+    * @var geoEncode
+    */
+    protected $geoEncoder;
+
+    /**
+    * @var Vendor
+    */
+    protected $vendor;
+
+    /**
+    * @var SimpleXMLElement
+    */
+    protected $xml;
+
+   /**
+    *
+    * @param Doctrine_Record $vendor
+    * @param SimpleXMLElement $xml
+    * @param geoEncode $geoEncoder
+    */
+    public function __construct( Doctrine_Record $vendor, SimpleXMLElement $xml, geoEncode $geoEncoder = null)
+    {
+
+        if( !$vendor )
+        {
+            // Find vendor
+            $vendor = Doctrine::getTable('Vendor')->findOneByCityAndLanguage( 'chicago', 'en-US' );
+        }
+        
+        if( !isset( $vendor ) || !$vendor )
+          throw new Exception( 'ChicagoFeedBaseMapper:: Vendor not found' );
+
+        // Set data
+        $this->geoEncoder           = is_null( $geoEncoder ) ? new geoEncode() : $geoEncoder;
+        $this->vendor               = $vendor;
+        $this->xml                  = $xml;
+    }
+
+    /**
+     * Get Nodes of given Xpath in $XML
+     * @param string $nodePath
+     * @param SimpleXMLElement $xml
+     * @return Array
+     */
+    protected function getXMLNodesByPath( $nodePath, SimpleXMLElement $xml = null )
+    {
+        if( !$xml )
+            $xml = $this->xml;
+
+        return $xml->xpath( $nodePath );
+    }
+}
+?>
