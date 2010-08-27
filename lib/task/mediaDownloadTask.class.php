f244ef7d75b407878131fb77a0af4835075a793e
Peter Johnson <peterjohnson@timeout.com>
2010-08-27 18:07:55 +0100

    refs #626 -- Updated mediaDownloadTask.

--- lib/task/mediaDownloadTask.class.php
+++ lib/task/mediaDownloadTask.class.php
@@ -30,101 +30,132 @@ EOF;
 
     // Valid File Types.
     $this->validFileTypes = array( 'image/jpeg' );
+
+    // Schedule of which id suffixes ie. id:113 = 3, id:1024 = 4, to check on which day.
+    // This is used to check existing media only once per week, spreading the load.
+    $this->schedule = array();
+    $this->schedule['Sunday']    = array( 0 );
+    $this->schedule['Monday']    = array( 1, 7 );
+    $this->schedule['Tuesday']   = array( 2 );
+    $this->schedule['Wednesday'] = array( 3, 8 );
+    $this->schedule['Thursday']  = array( 4 );
+    $this->schedule['Friday']    = array( 5, 9 );
+    $this->schedule['Saturday']  = array( 6 );
   }
 
   protected function execute( $arguments = array(), $options = array())
   {
     $this->setUp( $options );
-    $this->batchDownload( 'Poi' );
-    $this->batchDownload( 'Event' );
-    $this->batchDownload( 'Movie' );
+
+    // Download new Media.
+    $this->batchProcess( Doctrine::getTable( 'PoiMedia'   )->findByStatus( 'new' ) );
+    $this->batchProcess( Doctrine::getTable( 'EventMedia' )->findByStatus( 'new' ) );
+    $this->batchProcess( Doctrine::getTable( 'MovieMedia' )->findByStatus( 'new' ) );
+
+    // Check Media scheduled for today.
+    $this->batchProcess( Doctrine::getTable( 'PoiMedia' )->createQuery()
+        ->where( 'RIGHT( id, 1 ) IN ( ? )', implode( $this->schedule[ date('l') ] ) )
+        ->execute() );
+
+    $this->batchProcess( Doctrine::getTable( 'EventMedia' )->createQuery()
+        ->where( 'RIGHT( id, 1 ) IN ( ? )', implode( $this->schedule[ date('l') ] ) )
+        ->execute() );
+
+    $this->batchProcess( Doctrine::getTable( 'MovieMedia' )->createQuery()
+        ->where( 'RIGHT( id, 1 ) IN ( ? )', implode( $this->schedule[ date('l') ] ) )
+        ->execute() );
   }
 
-  protected function batchDownload( $model )
+  protected function batchProcess( Doctrine_Collection $collection )
   {
-    $media = Doctrine::getTable( $model . 'Media' )->findByStatus( 'new' );
-
-    foreach( $media as $m )
+    foreach( $collection as $media )
     {
-        if( $this->recordIsNew( $m ) || $this->recordHasChanged( $m ) )
+        if( $this->mediaIsNew( $media ) || $this->mediaHasChanged( $media ) )
         {
-            $this->download( $m );
+            $this->download( $media );
         }
     }
+    $collection->free( true );
+    unset( $collection );
   }
 
-  protected function recordIsNew( $record )
+  protected function mediaIsNew( Doctrine_Record $media )
   {
-      if( !isset( $record['url'] ) ) return false;
-      return ( $record['status'] == 'new' ) ? true : false;
+      return isset( $media['status'] ) && $media['status'] == 'new' ? true : false;
   }
 
-  protected function recordHasChanged( $record )
+  protected function mediaHasChanged( Doctrine_Record $media )
   {
-      if( !isset( $record['url'] ) ) return false;
+      if( !isset( $media['url'] ) ) return false;
 
-      $headers = $this->fetchFinalHeader( $record['url'] );
+      $headers = $this->fetchAuthoritativeHeader( $media['url'] );
 
-      return ( $headers['Content-Type']      != $record['mime_type'] ) ||
-             ( $headers['Content-Length']    != $record['content_length'] ) ||
-             ( $headers['ETag']              != $record['etag'] );
+      return ( isset( $headers['Content-Type'] )   && $headers['Content-Type']   != $media['mime_type'] ) ||
+             ( isset( $headers['Content-Length'] ) && $headers['Content-Length'] != $media['content_length'] ) ||
+             ( isset( $headers['ETag'] )           && $headers['ETag']           != $media['etag'] );
   }
 
-  protected function download( $record )
+  protected function download( Doctrine_Record $media )
   {
-      if( !isset( $record['Poi']['Vendor']['city'] ) || !isset( $record['ident'] ) || !isset( $record['url'] ) )
+      if( !isset( $media['Poi']['Vendor']['city'] ) || !isset( $media['ident'] ) || !isset( $media['url'] ) )
           return;
 
-      $type         = strtolower( str_replace( 'Media', '', get_class( $record ) ) );
-      $city         = str_replace( ' ', '_', $record['Poi']['Vendor']['city'] );
-      $destination  = sfConfig::get( 'sf_root_dir' ) . "/import/{$city}/{$type}/media/{$record['ident']}.jpg";
+      $type         = strtolower( str_replace( 'Media', '', get_class( $media ) ) );
+      $city         = str_replace( ' ', '_', $media['Poi']['Vendor']['city'] );
+      $destination  = sfConfig::get( 'sf_root_dir' ) . "/import/{$city}/{$type}/media/{$media['ident']}.jpg";
       
-      $curl = new Curl( $record['url'] );
+      $curl = new Curl( $media['url'] );
       $curl->downloadTo( $destination );
 
-      $record['mime_type']          = $curl->getContentType();
-      $record['file_last_modified'] = $curl->getLastModified();
-      $record['etag']               = $curl->getETag();
-      $record['content_length']     = $curl->getContentLength();
-      $record['status']             = 'valid';
-      $record['last_header_check']  = date("Y-m-d H:i:s");
+      $media['mime_type']          = $curl->getContentType();
+      $media['file_last_modified'] = $curl->getLastModified();
+      $media['etag']               = $curl->getETag();
+      $media['content_length']     = $curl->getContentLength();
+      $media['status']             = 'valid';
+      $media['last_header_check']  = date("Y-m-d H:i:s");
 
       try {
-        $this->validateDownload( $curl, $destination, $record );
+        $this->validateDownload( $curl, $destination, $media );
       }
-      catch( MediaException $e )
+      catch( MediaException $exception )
       {
-        $record['mime_type']          = NULL;
-        $record['file_last_modified'] = NULL;
-        $record['etag']               = NULL;
-        $record['content_length']     = NULL;
-        $record['status']             = 'error';
-        $record->save();
+        $media['mime_type']          = NULL;
+        $media['file_last_modified'] = NULL;
+        $media['etag']               = NULL;
+        $media['content_length']     = NULL;
+        $media['status']             = 'error';
+        $media->save();
 
-        throw $e;
+        throw $exception; // Re-throw Exception
       }
 
-      $record->save();
+      $media->save();
   }
 
-  protected function validateDownload( $curl, $destination, $record )
+  protected function validateDownload( Curl $curl, $destination, Doctrine_Record $media )
   {
       $curlInfo             = $curl->getCurlInfo();
       $responseCode         = $curlInfo[ 'http_code' ];
+      $imageDimensions      = @getimagesize( $destination );
 
       switch( false )
       {
           case $responseCode == 200 /* success */ || $responseCode == 304 /* not modified */ :
               $errorMessage = "Invalid HTTP Code: '{$responseCode}'"; break;
 
-          case in_array( $record['mime_type'], $this->validFileTypes ) :
-              $errorMessage = "Invalid MIME Type: '{$record['mime_type']}'"; break;
-
-          case is_numeric( $record['content_length'] ) && $record['content_length'] > 0 :
-              $errorMessage = "Invalid Byte Length: '{$record['content_length']}'"; break;
-
           case file_exists( $destination ) :
               $errorMessage = "Failed to Save to Destination: '{$destination}'"; break;
+
+          case in_array( mime_content_type( $destination ), $this->validFileTypes ) :
+          case in_array( $media['mime_type'], $this->validFileTypes ) :
+              $errorMessage = "Invalid MIME Type: '{$media['mime_type']}'"; break;
+
+          case is_numeric( $media['content_length'] ) && $media['content_length'] > 0 :
+              $errorMessage = "Invalid Byte Length: '{$media['content_length']}'"; break;
+
+          case isset( $imageDimensions[0] ) && is_numeric( $imageDimensions[0] ) && $imageDimensions[0] > 0 :
+          case isset( $imageDimensions[1] ) && is_numeric( $imageDimensions[1] ) && $imageDimensions[1] > 0 :
+             $errorMessage = "Invalid Image Size: '{$imageDimensions[0]}x{$imageDimensions[1]}'"; break;
 
           default : $errorMessage = '';
       }
@@ -132,14 +163,14 @@ EOF;
       if( !empty( $errorMessage ) )
       {
         unlink( $destination );
-        throw new MediaException( "{$errorMessage} for ".get_class( $record )." id:".$record['id'] );
+        throw new MediaException( "{$errorMessage} for ".get_class( $media )." id:".$media['id'] );
       }
   }
 
   /**
    * Get the last set of headers, removing redirect headers.
    */
-  protected function fetchFinalHeader( $url )
+  protected function fetchAuthoritativeHeader( $url )
   {
       $headers = get_headers( $url, 1 );
       if( $headers === false ) return array();
@@ -152,10 +183,11 @@ EOF;
           }
           if( is_numeric( $key ) )
           {
-            $headers[ 'Status-Code' ] = preg_replace( "/[^0-9]/", "", $value );
+            preg_match( "/\s([0-9]{3})\s/", $value, $matches );
+            $headers[ 'Status-Code' ] = array_pop( $matches );
+            unset( $headers[ $key ] );
           }
       }
-
       return $headers;
   }
 }
