0aa22414536c0588b6aa76c44e22d0801ec2a530
Rajeevan <rajeevankumarathasan@timeout.com>
2010-09-01 15:52:42 +0100

    refs #565 - Movie Mapper class created

---
+++ lib/import/singapore/singaporeMovieMapper.class.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Singapore movie mapper
+
+ *
+ * @package projectn
+ * @subpackage singapore.import.lib.unit
+ *
+ * @author Rajeevan Kumarathasan <rajeevankumarathasan@timout.com>
+ * @copyright Timeout Communications Ltd
+ *
+ * @version 1.0.0
+ *
+ */
+class singaporeMovieMapper extends DataMapper
+{
+    public function __construct( SimpleXMLElement $xml, geoEncode $geoEncoder = null )
+    {
+        $this->vendor     = Doctrine::getTable( 'Vendor' )->findOneByCityAndLanguage( 'singapore', 'en-US' );
+        $this->geoEncoder = is_null( $geoEncoder ) ? new geoEncode() : $geoEncoder;
+        $this->xml        = $xml;
+    }
+
+    public function mapMovie()
+    {
+        foreach( $this->xml->movie as $movieNode)
+        {
+            try{
+                $vendorMovieID  = (string) $movieNode->id;
+                // Get Existing Movie Element or Create new
+                $movie = Doctrine::getTable( 'Movie' )->findOneByVendorMovieIdAndVendorId( $vendorMovieID, $this->vendor[ 'id' ] );
+                if( !$movie )
+                {
+                    $movie = new Movie();
+                }
+
+                // Map Data
+                $movie[ 'vendor_id' ] = $this->vendor[ 'id' ];
+                $movie[ 'vendor_movie_id' ] = (string) $movieNode->id;
+                $movie[ 'name' ] = (string) $movieNode->title;
+                $movie[ 'review' ] = (string) $movieNode->synopsis;
+                $movie[ 'url' ] = (string) (string) $movieNode->website;
+                $movie[ 'director' ] = (string) $movieNode->director;
+                $movie[ 'cast' ] = (string) $movieNode->cast;
+                $movie[ 'release_date' ] = (string) $movieNode->opens;
+                $movie[ 'duration' ] = (string) $movieNode->length;
+                $movie[ 'country' ] = (string) $movieNode->origin;
+                $movie[ 'age_rating' ] = $this->getAgeRatingCode( (string) $movieNode->certificate);
+
+                $movie[ 'utf_offset' ] = $this->vendor->getUtcOffset();
+
+                //genres
+                if ( (string) $movieNode->category != '' )
+                {
+                    $movie->addGenre( (string) $movieNode->category );
+                }
+
+                //properties
+                $movie->setCriticsChoiceProperty( ( trim( strtolower( (string) $movieNode->critic_choice ) ) == 'y' ) ? true : false );
+                $movie->setTimeoutLinkProperty( (string) $movieNode->link );
+                
+                if ( (string) $movieNode->trailer_url != '' ) $movie->addProperty( 'Trailer_url', (string) $movieNode->trailer_url );
+                if ( (string) $movieNode->certificate != '' ) $movie->addProperty( 'Certificate', (string) $movieNode->certificate );
+                if ( (string) $movieNode->year_production != '' ) $movie->addProperty( 'year', (string) $movieNode->year_production );
+
+                // -- Add Images --
+                $this->addImageHelper( $movie , (string)$movieNode->highres );
+                $this->addImageHelper( $movie , (string)$movieNode->large_image );
+                $this->addImageHelper( $movie , (string)$movieNode->thumbnail );
+                $this->addImageHelper( $movie , (string)$movieNode->image );
+                $this->addImageHelper( $movie , (string)$movieNode->thumb );
+
+                // Save data
+                $this->notifyImporter( $movie );
+
+            }
+            catch( Exception $exception )
+            {
+                $this->notifyImporterOfFailure( new Exception ( 'Exception singaporeMovieMapper::mapMovie - ' . $exception->getMessage() ) );
+                echo 'Exception singaporeMovieMapper::mapMovie - ' . $exception->getMessage() . PHP_EOL;
+            }
+        }
+    }
+
+    /**
+     * Extract Age Rating Code from String
+     * @param string $stringCertificate
+     * @return string
+     */
+    private function getAgeRatingCode( $stringCertificate )
+    {
+        $ageratingArray = explode( '-',  $stringCertificate );
+        $ageratingCodeString = trim( $ageratingArray[ 0 ] );
+
+        if ( in_array( $ageratingCodeString, array( 'G', 'PG', 'NC16', 'M18', 'R18', 'R21' ) ) )
+        {
+            return $ageratingCodeString;
+        }
+
+        return null;
+    }
+}
+?>
