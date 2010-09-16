8cf327686d6ff822f6451d16084245ff6837f73d
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-09-16 17:10:27 +0100

    #refs 655 add zipping

--- scripts/duplicate_india_movies.sh
+++ scripts/duplicate_india_movies.sh
@@ -5,3 +5,8 @@ cd /var/vhosts/projectn/httpdocs/export/export_$(date +"%Y%m%d")/movie/
 sed 's/\(<movie id="\)BOM\([0-9]\+" .*>\)/\1DEL\2/g' mumbai.xml > delhi.xml
 sed 's/\(<movie id="\)BOM\([0-9]\+" .*>\)/\1BLR\2/g' mumbai.xml > bangalore.xml
 sed 's/\(<movie id="\)BOM\([0-9]\+" .*>\)/\1PNQ\2/g' mumbai.xml > pune.xml
+
+cd /var/vhosts/projectn/httpdocs/export/
+
+tar zcvf exports_$(date +"%Y%m%d").tgz export_$(date +"%Y%m%d")/*
+
