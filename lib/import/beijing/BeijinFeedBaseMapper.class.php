db632f6e11099fd1dde4e8bbfa80e7bf755d4c7e
Rajeevan <rajeevankumarathasan@timeout.com>
2010-10-06 12:50:50 +0100

    refs #717 - beijing Basemapper, Beijing Mapper and test

--- lib/import/beijing/BeijinFeedBaseMapper.class.php
+++ lib/import/beijing/BeijinFeedBaseMapper.class.php
@@ -15,61 +15,71 @@
 class BeijingFeedBaseMapper extends DataMapper
 {
     /**
-     * @var PDO Database Connection Object
-     */
-    protected $pdoDB;
-
-    /**
-    * @var geocoder
-    */
-    protected $geocoderr;
-
-    /**
     * @var Vendor
     */
     protected $vendor;
+
+    /**
+     * @var xmlNodes
+     */
+    protected $xmlNodes;
+
+    /**
+     * @var params
+     */
+    protected $params;
 
     /**
      * Create Base Mapper
      * @param PDO $pdoDB (Connected object)
      * @param geocoder $geocoderr
      */
-    public function  __construct( $pdoDB, geocoder $geocoderr = null ) {
-
-        if( is_null($pdoDB) )
-            throw new Exception ('Invalid PDO Database object');
-
-        // Get Vendor Beijing
-        $vendor = Doctrine::getTable('Vendor')->findOneByCityAndLanguage( 'beijing', 'en-GB' );
+    public function  __construct( Vendor $vendor, $params ) {
 
         if( !isset( $vendor ) || !$vendor )
-          throw new Exception( 'Vendor not found.' );
+        {
+            throw new Exception( 'Vendor not found.' );
+        }
 
+        // Validate Params
+        if( is_null($params) || !is_array($params) || count($params) <= 0 )
+        {
+            throw new Exception( 'Parameter Required' );
+        }
 
-        $this->pdoDB        = $pdoDB; // Set DB
+        // @todo: create Exception Base an throw all error in one Exception
+        // validate datasource
+        if( !isset( $params['datasource'] ) || !is_array( $params['datasource'] ) || count( $params['datasource'] ) <= 0 )
+        {
+            throw new Exception( 'Parameter missing datasource configuration' );
+        }
+
+        if( !isset( $params['datasource']['classname'] )  || empty( $params['datasource']['classname'] ) )
+        {
+            throw new Exception( 'Parameter missing datasource ClassName' );
+        }
+
+        if( !isset( $params['datasource']['url'] )  || empty( $params['datasource']['url'] ) )
+        {
+            throw new Exception( 'Parameter missing datasource URL' );
+        }
+
+        if( !isset( $params['datasource']['username'] )  || empty( $params['datasource']['username'] ) )
+        {
+            throw new Exception( 'Parameter missing datasource username' );
+        }
+        
+        if( !isset( $params['datasource']['password'] )  || empty( $params['datasource']['password'] ) )
+        {
+            throw new Exception( 'Parameter missing datasource password' );
+        }
+
         $this->vendor       = $vendor; // Set Vendor
-        $this->geocoderr   = is_null( $geocoderr ) ? new googleGeocoder() : $geocoderr;
+        $this->params       = $params;
+
+        // Fetch the Data
+        $this->getXMLFeed();
     }
-
-    protected function query( $tableName, $offset = 0, $limit = 500, $cityID = 2 )
-    {
-        if( !$this->pdoDB )
-        {
-            throw new Exception ('Invalid PDO Database object');
-        }
-
-        $query = null;
-        try{
-            $query = $this->pdoDB->query( 'SELECT * FROM ' . $tableName . ' LIMIT '. $offset . ', '. $limit );
-            
-        } catch ( Exception $exception )
-        {
-            throw new Exception( ' PDO Query Error: ' . $exception->getMessage() . PHP_EOL );
-        }
-
-        return $query;
-    }
-
 
     protected function fixHtmlEntities( $string )
     {
@@ -93,6 +103,93 @@ class BeijingFeedBaseMapper extends DataMapper
     protected function clean( $string , $chars = '' )
     {
         return stringTransform::mb_trim( $string, $chars );
+    }
+
+    /**
+     * Beijing chinese feed require authentication before you can download the Feed,
+     * authentication is done through screen Scraping login page and submiting with Login value.
+     * curl class is utilized to do this and the feed download page checking authentication by cookie,
+     * it is important to pass a cookie file
+     */
+    protected function getXMLFeed()
+    {
+        // Set temporary Cookie File
+        $cookieFile = tempnam ("/tmp", "CURLCOOKIE");
+
+        // Get the Login Form with its view state and ect other asp.net hidden fields
+        $curl = new $this->params['datasource']['classname']( $this->params['datasource']['url'] ); // "http://www.timeoutcn.com/Account/Login.aspx?ReturnUrl=/admin/n/london/Default.aspx"
+        $curl->setCurlOption(CURLOPT_COOKIEJAR, $cookieFile);
+        $curl->exec();
+
+        // Get the Login page HTML and extract all input fields
+        $loginPageHTML = $curl->getResponse();
+
+        // Remove all news lines, as it's easier to Preg match
+        $newlines = array("\t","\n","\r","\x20\x20","\0","\x0B");
+        $loginPageHTML = str_replace($newlines, "", html_entity_decode($loginPageHTML));
+
+        // Extract all the Inputs
+        preg_match_all("|<input.*/>|U",$loginPageHTML, $Loginfields);
+
+        if( !is_array( $Loginfields ) || empty( $Loginfields ) )
+        {
+            throw new Exception( 'Failed to Extract Login fields from login form at ' . $this->params['datasource']['url'] );
+        }
+
+        // Set default Fields Expected
+        $fields = array( '__EVENTTARGET' => '', '__EVENTARGUMENT' => '', '__EVENTVALIDATION' => '', 'ctl00$CM$Login1$UserName' => '', 'ctl00$CM$Login1$Password' => '', 'ctl00$CM$Login1$LoginButton' => '', 'ctl00$CM$Login1$RememberMe' => '' );
+
+        // Extract the name & value of the Fields
+        foreach( $Loginfields[0] as $input )
+        {
+            preg_match( '/<input.*?name\\s*=\\s*"?([^\\s>"]*).*?value\\s*=\\s*"?([^\\s"]*).*?/i', $input, $match);
+            if( count($match) == 3 )
+            {
+                $fields[ $match[1] ] = $match[2];
+            }
+        }
+
+        // Set username / Password
+        $fields[ 'ctl00$CM$Login1$UserName' ] = $this->params['datasource']['username'];
+        $fields[ 'ctl00$CM$Login1$Password' ] = $this->params['datasource']['password'];
+
+        // Overriders / Simulate Javascript Click event
+        $fields[ '__EVENTTARGET' ] = 'ctl00$CM$Login1$LoginButton';
+        $fields[ '__EVENTARGUMENT' ] = '';
+
+        echo 'Downloading XML' . PHP_EOL;
+
+        // Send the request as POST to authenticate and download the Feed
+        $curl = new $this->params['datasource']['classname']( $this->params['datasource']['url'], $fields, "POST");
+        $curl->setCurlOption(CURLOPT_COOKIEFILE, $cookieFile); // Give the Cookie File that already written in previous request
+        $curl->exec();
+
+        // get the Response
+        $response = $curl->getResponse();
+        
+        // Validate XML
+        if( !$this->isValidResponse( str_replace($newlines , "", $response ) ) )
+        {
+            throw new Exception( 'Failed to download the feed, response to pass valid feed test...' );
+            var_dump( $response );
+        }
+
+        // Parse as XML
+        $this->xmlNodes = simplexml_load_string( $response );
+        
+        // [Safe] Delete the Cookie file
+        @unlink($cookieFile);
+    }
+
+    /**
+     * Validate the Response after authentication, Require to Override,
+     * default allways return fasle!
+     * @param string $response
+     * @return boolean
+     */
+    protected function isValidResponse( $response )
+    {
+        return false;
     }
 }
 ?>
--- test/unit/lib/import/beijing_zh/beijingZHFeedPoiMapperTest.php
+++ test/unit/lib/import/beijing_zh/beijingZHFeedPoiMapperTest.php
@@ -2,7 +2,7 @@
 require_once 'PHPUnit/Framework.php';
 require_once dirname( __FILE__ ) . '/../../../../../test/bootstrap/unit.php';
 require_once dirname( __FILE__ ) . '/../../../bootstrap.php';
-
+require_once TO_TEST_MOCKS . '/curl.mock.php';
 /**
  * Test of beijing Venue Mapper
  *
@@ -44,7 +44,65 @@ class beijingZHFeedPoiMapperTest extends PHPUnit_Framework_TestCase
       ProjectN_Test_Unit_Factory::destroyDatabases();
   }
 
-  public function testFeedDownload()
+  public function testMapVenue()
+  {
+      $params = array( 'datasource' => array( 'classname' => 'CurlMock', 'url' => TO_TEST_DATA_PATH . '/beijing/beijing_zh.venue.xml', 'username' => 'tolondon' , 'password' => 'to3rjk&e*8dsfj9' ) );
+
+      $dataMapper = new beijingZHFeedVenueMapper( $this->vendor, $params );
+
+      $importer = new Importer();
+      $importer->addDataMapper($dataMapper);
+      $importer->run();
+
+      // Get all the POI's
+      $pois =  Doctrine::getTable( 'Poi' )->findAll();
+
+      $this->assertEquals( 6, $pois->count(), 'There should be 6 Pois added' );
+
+      // get 1 and Test
+      $poi = $pois[0];
+
+      $this->assertEquals( '3', $poi['vendor_poi_id'], 'Invalid vendor Poi ID');
+      $this->assertEquals( '俏江南', $poi['name'], 'Wrong Poi Name');
+      $this->assertEquals( '1号', $poi['house_no'], 'Wrong Poi House No');
+      $this->assertEquals( '东城区东长安街', $poi['street'], 'Wrong Poi street');
+      $this->assertEquals( '东城', $poi['district'], 'Wrong Poi district');
+      $this->assertEquals( '100010', $poi['zips'], 'Wrong Poi zips');
+      $this->assertEquals( '116.414518', $poi['latitude'], 'Wrong Poi latitude');
+      $this->assertEquals( '39.909807', $poi['longitude'], 'Wrong Poi longitude');
+      $this->assertEquals( '东城区东长安街1号东方广场B1楼BB88号', $poi['short_description'], 'Wrong Poi short_description');
+      $this->assertEquals( '东城区东长安街1号东方广场B1楼BB88号', $poi['description'], 'Wrong Poi description');
+
+      $this->assertEquals( '+86 8 518 6971', $poi['phone'], 'Wrong Poi Phone Number');
+
+      // timeout URL
+      $this->assertEquals( 1, $poi['PoiProperty']->count() );
+      $this->assertEquals( 'http://www.timeoutcn.com/Articles_11_14.htm', $poi['PoiProperty'][0]['value'] );
+
+      // assert another
+      $poi = $pois[4];
+
+      $this->assertEquals( '7', $poi['vendor_poi_id'], 'Invalid vendor Poi ID');
+      $this->assertEquals( '玉', $poi['name'], 'Wrong Poi Name');
+      $this->assertEquals( '甲83号', $poi['house_no'], 'Wrong Poi House No');
+      $this->assertEquals( '朝阳区建国路', $poi['street'], 'Wrong Poi street');
+      $this->assertEquals( '朝阳', $poi['district'], 'Wrong Poi district');
+      $this->assertEquals( '朝阳区建国路甲83号丽思卡尔顿酒店', $poi['additional_address_details'], 'Wrong Poi additional_address_details');
+      $this->assertEquals( '100020', $poi['zips'], 'Wrong Poi zips');
+      $this->assertEquals( '116.5416677', $poi['latitude'], 'Wrong Poi latitude');
+      $this->assertEquals( '39.9081711', $poi['longitude'], 'Wrong Poi longitude');
+      $this->assertEquals( '以前，酒店里的中餐馆都给人又贵又不靠谱的印象，从半岛的凰庭、君悦的长安一号开始，酒店里的中餐馆也有了...', $poi['short_description'], 'Wrong Poi short_description');
+      $this->assertStringStartsWith( '以前，酒店里的中餐馆都给', $poi['description'], 'Wrong Poi description');
+
+      $this->assertEquals( '+86 5 908 8888', $poi['phone'], 'Wrong Poi Phone Number');
+
+      // timeout URL
+      $this->assertEquals( 1, $poi['PoiProperty']->count() );
+      $this->assertEquals( 'http://www.timeoutcn.com/Articles_12_15.htm', $poi['PoiProperty'][0]['value'] );
+      
+  }
+
+  public function _testFeedDownload()
   {
         $cookieFile = tempnam ("/tmp", "CURLCOOKIE");
 
@@ -86,9 +144,7 @@ class beijingZHFeedPoiMapperTest extends PHPUnit_Framework_TestCase
         $curl->setCurlOption(CURLOPT_COOKIEFILE, $cookieFile);
         $curl->exec();
 
-        file_put_contents( '/n/beijing_zh.xml', $curl->getResponse());
-
-        var_dump(file_get_contents( $cookieFile ));
+        var_dump( $curl->getHeader() );
         unlink($cookieFile);
   }
 }
