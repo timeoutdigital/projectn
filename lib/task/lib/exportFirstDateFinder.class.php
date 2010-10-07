5d3f741297110253bacec7077b9f521f22152bb9
Clarence <clarencelee@timeout.com>
2010-10-07 13:53:04 +0100

    refs #728 added class and task to check first date of exports from a download of export backups, should add functionality to download from s3 automatically

---
+++ lib/task/lib/exportFirstDateFinder.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class exportFirstDateFinder
+{
+  private $exportType;
+  private $pathToExports;
+
+  public function __construct( $exportType, $pathToExports )
+  {
+    $this->exportType    = $exportType;
+    $this->pathToExports = $pathToExports;
+  }
+
+  public function run()
+  {
+    $firstExportDates = array();
+
+    foreach( DirectoryIteratorN::iterate( $this->pathToExports, DirectoryIteratorN::DIR_FOLDERS ) as $dirName )
+    {
+      $date = new DateTime( str_replace( 'export_', '', $dirName ) );
+
+      $absolutePath = implode( '/', array( $this->pathToExports,
+                                           $dirName,
+                                           $this->exportType,
+                                         ) );
+
+      foreach( DirectoryIteratorN::iterate( $absolutePath, DirectoryIteratorN::DIR_FILES, 'xml' ) as $fileName )
+      {
+        $cityName =  str_replace( '_', ' ', array_shift( explode( '.', $fileName ) ) );
+
+        if( !isset( $firstExportDates[ $cityName ] ) )
+        {
+          $firstExportDates[ $cityName ] = $date;
+        }
+      }
+    }
+
+    return $firstExportDates;
+  }
+}
