f2b782fc8f94e061f08d10aebf1e8d4b7aa6c82a
Rajeevan <rajeevankumarathasan@timeout.com>
2010-10-11 11:45:48 +0100

    refs #723 - MS Word clean up, Method is created to download the XMl feed an apply MS word Clean to entire document by default. This method can be overritten to control where to apply MS Word clean up... See venues for example

--- lib/import/hong_kong/HongKongFeedBaseMapper.class.php
+++ lib/import/hong_kong/HongKongFeedBaseMapper.class.php
@@ -14,16 +14,11 @@
 class HongKongFeedBaseMapper extends DataMapper
 {
     /**
-    *
-    * @var projectNDataMapperHelper
-    */
-    protected $dataMapperHelper;
-
-    /**
-    * @var geoEncode
-    */
-    protected $geoEncoder;
-
+     *
+     * @var array
+     */
+    protected $params;
+    
     /**
     * @var Vendor
     */
@@ -40,23 +35,56 @@ class HongKongFeedBaseMapper extends DataMapper
     * @param geoEncode $geoEncoder
     * @param string $city
     */
-    public function __construct( SimpleXMLElement $xml, geoEncode $geoEncoder = null )
+    public function __construct( Vendor $vendor, $params)
     {
-       
-        $vendor = Doctrine::getTable('Vendor')->findOneByCityAndLanguage( 'hong kong', 'en-HK' );
-
         if( !isset( $vendor ) || !$vendor )
-          throw new Exception( 'Vendor not found.' );
+        {
+            throw new Exception( 'Vendor not found.' );
+        }
 
-        $this->geoEncoder           = is_null( $geoEncoder ) ? new geoEncode() : $geoEncoder;
+        // validate Params / Datasource
+        if( !is_array( $params ) || count( $params ) <= 0 )
+        {
+            throw new Exception( ' invalid Parameter, should be an array' );
+        }
+
+        // validate data source / url
+        if( !isset( $params['datasource'] ) || !is_array($params['datasource']) || empty( $params['datasource'] ) )
+        {
+            throw new Exception( 'No Datasource specified' );
+        }
+        if( !isset( $params['datasource']['url'] ) || trim( $params['datasource']['url'] ) == '' )
+        {
+            throw new Exception( 'Invaid Datasource URL specified' );
+        }
+        if( !isset( $params['datasource']['classname'] ) || trim( $params['datasource']['classname'] ) == '' )
+        {
+            throw new Exception( 'Invalid Datasource Classname specified' );
+        }
+
         $this->vendor               = $vendor;
-        $this->xml                  = $xml;
+        $this->params               = $params;
+
+        // Download the Feed
+        $this->getXMlFeed();
     }
 
     protected function fixHtmlEntities( $string )
     {
-        $string = html_entity_decode( (string) $string, ENT_QUOTES, 'UTF-8' );
+        // Remove Multiple lines
+        $string = stringTransform::removeMultipleLines( $string );
 
+        // HTML Decode
+        $string = html_entity_decode( (string) $string, ENT_QUOTES, 'UTF-8' );
+        
+        // Remove everything inside table and table tag
+        $string = mb_ereg_replace( '</?table[^>]*>(.+</table[^>]*>|)', '', $string );
+        
+        // Remove Multiple BR
+        /*$string = mb_ereg_replace("(<br\s*\/?>\s*)+", "<br/>", $string);*/
+
+        // Remove Empty Tags
+        // $string = preg_replace('|<(\w+)[^>]*></\1>|','', $string);
         return $string;
     }
 
@@ -92,6 +120,35 @@ class HongKongFeedBaseMapper extends DataMapper
                 $this->notifyImporterOfFailure( $e );
             }
         }
+    }
+
+    /**
+     * Download XML Feed from url in params Variable
+     */
+    protected function getXMlFeed()
+    {
+        // Get the Feed
+        $curl = new $this->params['datasource']['classname']( $this->params['datasource']['url'] );
+        $curl->exec();
+
+        // Go through Cleaning process and Parse it as XML
+        $xmlDataFixer = new xmlDataFixer( $curl->getResponse() );
+
+        // Call override cleaner
+        $this->getXMLFeedCleanUp( $xmlDataFixer );
+        
+        $xmlDataFixer->removeHtmlEntiryEncoding();
+        $xmlDataFixer->encodeUTF8();
+        $this->xml = $xmlDataFixer->getSimpleXML(); // get the XMl file and Set it in Class Variable
+    }
+
+    /**
+     * Override this function to Filter Mapper specific fields
+     * @param xmlDataFixer $xmlDataFixer
+     */
+    protected function getXMLFeedCleanUp( xmlDataFixer $xmlDataFixer )
+    {
+        $xmlDataFixer->removeMSWordHtmlTags();
     }
 }
 ?>
