a66b430ca4d5a04208e769b2d08a4b82a089a2ec
Rajeevan <rajeevankumarathasan@timeout.com>
2010-10-20 11:39:39 +0100

    refs #748 remove hydrator class from Plugin/statictics

--- plugins/projectnStatisticsPlugin/modules/statistics/lib/form/MetricDimensionForm.class.php
+++ plugins/projectnStatisticsPlugin/modules/statistics/lib/form/MetricDimensionForm.class.php
@@ -16,25 +16,3 @@ class MetricDimensionForm extends BaseForm
     $this->widgetSchema->setFormFormatterName( 'list' );
   }
 }
-
-// -----------
-
-/**
- * Custom Hydrator that Hydrates Key Value Associative Array.
- */
-class Doctrine_Hydrator_KeyValue extends Doctrine_Hydrator_Abstract
-{
-    public function hydrateResultSet($stmt)
-    {
-        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        if( empty( $data ) ) return $data;
-
-        $keys = array_keys( $data[ 0 ] );
-        foreach( $data as $k => $v ) $fixed_data[ $v[ $keys[ 0 ] ] ] = $v[ $keys[ 1 ] ];
-
-        ksort( $fixed_data );
-        return $fixed_data;
-    }
-}
-
-Doctrine_Manager::getInstance()->registerHydrator( 'KeyValue', 'Doctrine_Hydrator_KeyValue' );
