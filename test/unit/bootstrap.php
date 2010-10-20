8168e6328e5e0e23b696df6e8b327bc4bce81197
Rajeevan <rajeevankumarathasan@timeout.com>
2010-10-20 18:31:15 +0100

    refs #748 - new fixtures for  LogImport, LogImportCount, LogImportError, LogExport models has been created

--- test/unit/bootstrap.php
+++ test/unit/bootstrap.php
@@ -101,6 +101,10 @@ class ProjectN_Test_Unit_Factory
       'vendoreventcategory' => 'VendorEventCategoryFixture',
       'vendorpoicategory'   => 'VendorPoiCategoryFixture',
       'logimport'           => 'LogImportFixture',
+      'logimportcount'      => 'LogImportCountFixture',
+      'logimporterror'      => 'LogImportErrorFixture',
+      'logexport'           => 'LogExportFixture',
+      'logexportcount'      => 'LogExportCountFixture',
     );
 
     $model = strtolower( $model );
@@ -154,6 +158,133 @@ class ProjectN_Test_Unit_Factory
   }
 }
 
+class LogImportErrorFixture
+{
+    static public function create( $data=null, $autoCreateRelatedObjects=true )
+    {
+        $importError = new LogImportError();
+        if( $autoCreateRelatedObjects )
+        {
+            if( Doctrine::getTable( 'LogImport' )->count() < 1  )
+            {
+                ProjectN_Test_Unit_Factory::add( 'LogImport' );
+            }
+        }
+
+        $default = array(
+            'model' => 'Poi',
+            'exception_class' => 'Exception',
+            'trace' => 'no trace details',
+            'message' => null,
+            'log' => 'test error log',
+            'serialized_object' => null,
+            'resolved' => 0,
+            'log_import_id' => 1,
+        );
+
+        if( is_array( $data ) )
+        {
+            $default = array_merge( $default, $data );
+        }
+        
+        $importError->fromArray( $default );
+        return $importError;
+    }
+}
+
+class LogExportCountFixture
+{
+    static public function create( $data=null, $autoCreateRelatedObjects=true )
+    {
+        $logCount = new LogExportCount();
+
+        if( $autoCreateRelatedObjects )
+        {
+            if( Doctrine::getTable( 'LogExport' )->count() < 1  )
+            {
+                ProjectN_Test_Unit_Factory::add( 'LogExport' );
+            }
+        }
+
+        $default = array(
+            'model' => 'Poi',
+            'count' => 0,
+            'log_export_id' => 1,
+        );
+
+        if( is_array( $data ) )
+        {
+            $default = array_merge( $default, $data );
+        }
+
+        $logCount->fromArray( $default );
+
+        return $logCount;
+    }
+}
+
+class LogExportFixture
+{
+    static public function create( $data=null, $autoCreateRelatedObjects=true )
+    {
+        $logExport = new LogExport();
+
+        if( $autoCreateRelatedObjects )
+        {
+            if( Doctrine::getTable( 'Vendor' )->count() < 1  )
+            {
+                ProjectN_Test_Unit_Factory::add( 'Vendor' );
+            }
+        }
+
+        $default = array(
+            'vendor_id' => 1,
+            'status' => 'success',
+            'total_time' => '00:00:01',
+        );
+
+        if( is_array( $data ) )
+        {
+            $default = array_merge( $default, $data );
+        }
+        $logExport->fromArray($default );
+
+        return $logExport;
+
+    }
+}
+
+class LogImportCountFixture{
+
+    static public function create( $data=null, $autoCreateRelatedObjects=true )
+    {
+        $logCount = new LogImportCount();
+
+        if( $autoCreateRelatedObjects )
+        {
+            if( Doctrine::getTable( 'LogImport' )->count() < 1  )
+            {
+                ProjectN_Test_Unit_Factory::add( 'LogImport' );
+            }
+        }
+
+        $default = array(
+            'model' => 'Poi',
+            'count' => 0,
+            'log_import_id' => 1,
+            'operation' => 'failed',
+        );
+
+        if( is_array( $data ) )
+        {
+            $default = array_merge( $default, $data );
+        }
+        
+        $logCount->fromArray( $default );
+        
+        return $logCount;
+    }
+}
 class LogImportFixture
 {
     static public function create( $data=null, $autoCreateRelatedObjects=true )
