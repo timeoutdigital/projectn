64c1388c827a9cf6d0ed1efe49d57009a0afccce
Peter Johnson <peterjohnson@timeout.com>
2010-10-26 18:25:14 +0100

    refs #749 - Added shell script to move archives to s3.

---
+++ scripts/archives_to_s3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ARCHIVE_DATE=`date +"%Y-%m-%d-%H%M%S"`
+
+cd /n/
+
+tar -cjvf vendor_feeds-${ARCHIVE_DATE}.tbz vendor_feeds
+s3cmd put vendor_feeds-${ARCHIVE_DATE}.tbz s3://timeout-projectn-backups/vendor_feeds/
+
+if [ `s3cmd ls s3://timeout-projectn-backups/vendor_feeds/vendor_feeds-${ARCHIVE_DATE}.tbz | wc -l | bc` -eq 1 ]; then
+    rm vendor_feeds-${ARCHIVE_DATE}.tbz
+    rm vendor_feeds/*
+fi
