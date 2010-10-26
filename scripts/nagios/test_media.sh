184416816ef1f561a48229f6b98806ae3f4dcc04
Peter Johnson <peterjohnson@timeout.com>
2010-10-26 17:26:47 +0100

    refs #669 - Updated updated test media shell script.

--- scripts/nagios/test_media.sh
+++ scripts/nagios/test_media.sh
@@ -27,6 +27,9 @@ HTTP_LOG="/n/log/media_with_http_headers.log"
 # Total HTTP Errors.
 HTTP_ERRORS=$( cat ${HTTP_LOG} | wc -l | bc )
 
+# Images Left On Prod Server (not Sync'd)
+TOTAL_IMAGES_LOCAL=$(find /n/import/ -name *.jpg | wc -l | bc)
+
 # --------------------------------------------------------------------------------------------------------
 
 function f_ok {
@@ -52,8 +55,8 @@ function f_unk {
 # --------------------------------------------------------------------------------------------------------
 
 # Output
-if [ ${S3_ERRORS} -gt 0 ] || [ ${HTTP_ERRORS} -gt 0 ] ; then
-    f_war "Media Report\n\nA total of ${S3_ERRORS} images from last night exports were not found on s3.\nA log has been stored in ${S3_LOG} on projectn.live.\n\nA total of ${HTTP_ERRORS} images from last night exports contained HTTP headers.\nA log has been stored in ${HTTP_LOG} on projectn.live."
+if [ ${S3_ERRORS} -gt 0 ] || [ ${HTTP_ERRORS} -gt 0 ] || [ ${TOTAL_IMAGES_LOCAL} -gt 0 ]; then
+    f_war "Media Report\n\nA total of ${S3_ERRORS} images from last night exports were not found on s3.\nA log has been stored in ${S3_LOG} on projectn.live.\n\nA total of ${HTTP_ERRORS} images from last night exports contained HTTP headers.\nA log has been stored in ${HTTP_LOG} on projectn.live.\n\nA total of ${TOTAL_IMAGES_LOCAL} images are currently being stored locally."
 fi
 
 f_ok "OK: All export media fine."
