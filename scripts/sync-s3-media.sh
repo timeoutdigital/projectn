d8f5c317b798b4ddb7f43ada06b60758c16dee5d
Peter Johnson <peterjohnson@timeout.com>
2010-10-26 16:46:54 +0100

    refs #669 - Updated sync-s3-media shell script.

--- scripts/sync-s3-media.sh
+++ scripts/sync-s3-media.sh
@@ -12,7 +12,7 @@ do
         CURRENT_DIR="`pwd`/"
 
         # Sync Media Files.
-        # s3cmd sync --acl-public --guess-mime-type $CURRENT_DIR s3://projectn/$VENDOR_DIR/$MODEL_DIR/
+        s3cmd sync --acl-public --guess-mime-type $CURRENT_DIR s3://projectn/$VENDOR_DIR/$MODEL_DIR/
 
         # Pull a List of Media on s3 and Delete Matching Images on the Hard Drive
         s3cmd ls s3://projectn/$VENDOR_DIR/$MODEL_DIR/media/ | cut -d'/' -f7 | xargs -I {} find /n/import/ -name {} | xargs -I {} rm {} 2> /dev/null
