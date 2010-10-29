c966f3049269934910803f5d163d4c74dca3b9e2
Rajeevan <rajeevankumarathasan@timeout.com>
2010-10-29 14:56:00 +0100

    refs #766 - Export model table updated with new function to get LogWithCount for Vendor + date range. Test file updated..

--- lib/model/doctrine/LogExportTable.class.php
+++ lib/model/doctrine/LogExportTable.class.php
@@ -20,4 +20,23 @@ class LogExportTable extends LogTable
 
         return $q->execute( array(), $doctrineHydrateType );
     }
+
+    /**
+     * Get LogExport and LogExportCount from specified vendor + date from - date to
+     * as mixed type (Doctrine_core::Hydrate_Type) result set
+     * @param int $vendorID
+     * @param string $dateFrom
+     * @param string $dateTo
+     * @param int $doctrineHydrateType
+     * @return Mixed
+     */
+    public function getLogExportWithCountRecords( $vendorID, $dateFrom, $dateTo, $doctrineHydrateType = Doctrine_Core::HYDRATE_RECORD )
+    {
+        $q = Doctrine::getTable( 'LogExport' )->createQuery('l')
+        ->leftJoin( 'l.LogExportCount lc ON l.id = lc.log_export_id' )
+        ->where( 'l.vendor_id=?', $vendorID )
+        ->addWhere( 'l.created_at BETWEEN ? AND ?', array( $dateFrom, $dateTo ) );
+
+        return $q->execute( array(), $doctrineHydrateType );
+    }
 }
