86200354073ab74a03603c3b10605eea5c3af1ce
Peter Johnson <peterjohnson@timeout.com>
2010-11-16 15:38:20 +0000

    refs #776 -  Added try,catch, updated tests. Added description and usage example.

--- lib/XmlConcatenator.class.php
+++ lib/XmlConcatenator.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Description
+ * XmlConcatenator - Concatenates Simple XML Files.
  *
  * @package projectn
  * @subpackage
@@ -11,35 +11,62 @@
  *
  * @version 1.0.0
  *
+ * @description
+ *
+ * The first simplexml file is used as a parent document, the additional simplexml
+ * documents are searched using the xpath string provided and appended to the parent.
+ *
+ * The default target for the copied nodes is the direct parent of the children
+ * in the parent document.
+ *
+ * @example
+ * 
+ * $xml1 = simplexml_load_string( '<root><parent><child id="1"><![CDATA[1]]></child></parent></root>' );
+ * $xml2 = simplexml_load_string( '<root><parent><child id="2"><![CDATA[2]]></child></parent></root>' );
+ *
+ * $concatXML = XmlConcatenator::concatXML( array( $xml1, $xml2 ), '//parent/child' );
+ * echo $concatXML->save();
+ *
+ * @result
+ * 
+ * <root><parent><child id="1"><![CDATA[1]]></child><child id="2"><![CDATA[2]]></child></parent></root>
+ * 
  */
 
 class XmlConcatenator
 {
     public static function concatXML( array $simpleXMLStack, /* string */ $xpath )
     {
-        $parseXpath = explode( '/', trim( $xpath, '/' ) );
+        try {
+            $parseXpath = explode( '/', trim( $xpath, '/' ) );
 
-        $returnDomObject = new DomDocument();
-        $returnDomObject->loadXML( $simpleXMLStack[ 0 ]->asXML() );
+            $returnDomObject = new DomDocument();
+            $returnDomObject->loadXML( $simpleXMLStack[ 0 ]->asXML() );
 
-        foreach( $simpleXMLStack as $k => $simpleXMLDocument )
-        {
-            if( $k === 0 ) continue;
-
-            $domObject = new DomDocument();
-            $domObject->loadXML( $simpleXMLDocument->asXML() );
-            $domXpath   = new domXPath( $domObject );
-            $xpathQuery = $domXpath->query( $xpath );
-            
-            for( $i = 0; $i < $xpathQuery->length; $i++ )
+            foreach( $simpleXMLStack as $k => $simpleXMLDocument )
             {
-                $parentElement = $parseXpath[ count( $parseXpath ) - 2 ];
-                $returnDomObject->getElementsByTagName( $parentElement )->item(0)
-                        ->appendChild( $returnDomObject->importNode( $xpathQuery->item( $i ), true ) );
-            } 
-        }
+                if( $k === 0 ) continue;
 
-        return simplexml_import_dom( $returnDomObject );
+                $domObject = new DomDocument();
+                $domObject->loadXML( $simpleXMLDocument->asXML() );
+                $domXpath       = new domXPath( $domObject );
+                $xpathQuery     = $domXpath->query( $xpath );
+                $parentElement  = $parseXpath[ count( $parseXpath ) - 2 ];
+
+                for( $i = 0; $i < $xpathQuery->length; $i++ )
+                {
+                    $returnDomObject->getElementsByTagName( $parentElement )->item(0)
+                            ->appendChild( $returnDomObject->importNode( $xpathQuery->item( $i ), true ) );
+                }
+            }
+
+            return simplexml_import_dom( $returnDomObject );
+        }
+        catch( Exception $e )
+        {
+            throw new XmlConcatenatorException( 'XmlConcatenator Error', null, $e );
+        }
     }
 }
-?>
+
+class XmlConcatenatorException extends Exception {}
--- test/unit/lib/XmlConcatenatorTest.php
+++ test/unit/lib/XmlConcatenatorTest.php
@@ -18,22 +18,40 @@ require_once dirname(__FILE__).'/../bootstrap.php';
 
 class XmlConcatenatorTest extends PHPUnit_Framework_TestCase
 {
+    private $xmlString1 = '<root><parent><child id="1"><![CDATA[1]]></child><child id="2"><![CDATA[2]]></child></parent></root>';
+    private $xmlString2 = '<root><parent><child id="3"><![CDATA[3]]></child><child id="4"><![CDATA[4]]></child></parent></root>';
+
+    public function setUp()
+    {
+        $this->xml1 = simplexml_load_string( $this->xmlString1 );
+        $this->xml2 = simplexml_load_string( $this->xmlString2 );
+    }
+
     public function testConcatXML()
     {
-        $xml1 = simplexml_load_string( '<root><parent><child id="1"><![CDATA[1]]></child><child id="2"><![CDATA[2]]></child></parent></root>' );
-        $xml2 = simplexml_load_string( '<root><parent><child id="3"><![CDATA[3]]></child><child id="4"><![CDATA[4]]></child></parent></root>' );
-
-        $this->assertEquals( 1, (string) $xml1->parent->child[ 0 ] );
-        $this->assertEquals( 2, (string) $xml1->parent->child[ 1 ] );
+        $this->assertEquals( 1, (string) $this->xml1->parent->child[ 0 ] );
+        $this->assertEquals( 2, (string) $this->xml1->parent->child[ 1 ] );
         
-        $this->assertEquals( 3, (string) $xml2->parent->child[ 0 ] );
-        $this->assertEquals( 4, (string) $xml2->parent->child[ 1 ] );
+        $this->assertEquals( 3, (string) $this->xml2->parent->child[ 0 ] );
+        $this->assertEquals( 4, (string) $this->xml2->parent->child[ 1 ] );
 
-        $concatXML = XmlConcatenator::concatXML( array( $xml1, $xml2 ), '//parent/child' );
+        $concatXML = XmlConcatenator::concatXML( array( $this->xml1, $this->xml2 ), '//parent/child' );
 
         $this->assertEquals( 1, (string) $concatXML->parent->child[ 0 ] );
         $this->assertEquals( 2, (string) $concatXML->parent->child[ 1 ] );
         $this->assertEquals( 3, (string) $concatXML->parent->child[ 2 ] );
         $this->assertEquals( 4, (string) $concatXML->parent->child[ 3 ] );
+    }
+
+    public function testThrowInvalidXpathException()
+    {
+        $this->setExpectedException( 'XmlConcatenatorException', null, 'Invalid xpath' );
+        XmlConcatenator::concatXML( array( $this->xml1, $this->xml2 ), '' );
+    }
+
+    public function testThrowInvalidSimpleXMLException()
+    {
+        $this->setExpectedException( 'XmlConcatenatorException', null, 'No simplexml elements provided' );
+        XmlConcatenator::concatXML( array(), '' );
     }
 }
