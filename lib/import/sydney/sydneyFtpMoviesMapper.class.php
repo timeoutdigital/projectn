d13b524184ec535cb48106fa1f06f9b9ae066be8
Rajeevan <rajeevankumarathasan@timeout.com>
2010-11-17 17:35:22 +0000

    refs #709 - Sydney Movie mapper updated to new Style importer

--- lib/import/sydney/sydneyFtpMoviesMapper.class.php
+++ lib/import/sydney/sydneyFtpMoviesMapper.class.php
@@ -6,75 +6,69 @@
  * @subpackage
  *
  * @author Peter Johnson <peterjohnson@timout.com>
+ * @author Rajeevan Kumarathasan <rajeevankumarathasan.com>
  * @copyright Timeout Communications Ltd
  *
  * @version 1.0.1
  *
  */
 
-class sydneyFtpMoviesMapper extends DataMapper
+class sydneyFtpMoviesMapper extends sydneyFtpBaseMapper
 {
-  /**
-   * @var SimpleXMLElement
-   */
-  private $feed;
-
-  /**
-   * @var projectnDataMapperHelper
-   */
-  private $dataMapperHelper;
-
-  /**
-   * @var Vendor
-   */
-  private $vendor;
-
-  /**
-   * @param SimpleXMLElement $feed
-   */
-
-  public function __construct( Vendor $vendor, SimpleXMLElement $feed )
-  {
-    $this->feed = $feed;
-    $this->vendor = $vendor;
-    $this->dataMapperHelper = new projectnDataMapperHelper( $vendor );
-  }
 
   public function mapMovies()
   {
     foreach( $this->feed->film as $film )
     {
+        // We should rmeove this. I think!
         if( $this->insertedMoreThanThreeMonthsAgo( $film ) )
         {
           continue;
         }
-        $movie = $this->dataMapperHelper->getMovieRecord( (string) $film->EventID );
-        $movie['Vendor']            = $this->vendor;
-        $movie['vendor_movie_id']   = (string) $film->EventID;
-        $movie['name']              = (string) $film->Name;
-        //$movie['plot']              = (string) $film->Description;
-        //$movie['tag_line']          = (string);
-        $movie['review']            = (string) $film->Description;
-        $movie['url']               = (string) $film->Website;
-        $movie['director']          = (string) $film->FilmDirector;
-        //$movie['writer']            = (string);
-        $movie['cast']              = (string) $film->FilmActors;
-        $movie['age_rating']        = (string) $film->FilmOflcRating;
-        //$movie['release_date']      = (string) $film->FilmYear;
-        $movie['duration']          = (string) $film->FilmLength;
-        $movie['country']           = (string) $film->FilmCountry;
-        //$movie['language']          = (string);
-        //$movie['aspect_ratio']      = (string);
-        //$movie['sound_mix']         = (string);
-        //$movie['company']           = (string);
-        //$movie['rating']            = (string);
-        $movie['utf_offset']        = (string) $this->vendor->getUtcOffset();
 
-        //#753 addImageHelper capture Exception and notify, this don't break the Import process
-        $this->addImageHelper( $movie, (string) $film->ImagePath );
-       
+        try
+        {
+            // Get Existing movie or Create NEW
+            $vendor_movie_id = (string) $film->EventID; // Sydney sending movie like Event! and sometime events in movies, which should be Notified to vendor!
+            $movie = Doctrine::getTable( 'Movie' )->findOneByVendorIdAndVendorMovieId( $this->vendor['id'], $vendor_movie_id );
+            if( $movie === false )
+            {
+                $movie = new Movie();
+            }
 
-        ImportLogger::saveRecordComputeChangesAndLog( $movie );
+            // Map data
+            $movie['Vendor']            = $this->vendor;
+            $movie['vendor_movie_id']   = $vendor_movie_id;
+            $movie['name']              = (string) $film->Name;
+            //$movie['plot']              = (string) $film->Description;
+            //$movie['tag_line']          = (string);
+            $movie['review']            = (string) $film->Description;
+            $movie['url']               = (string) $film->Website;
+            $movie['director']          = (string) $film->FilmDirector;
+            //$movie['writer']            = (string);
+            $movie['cast']              = (string) $film->FilmActors;
+            $movie['age_rating']        = (string) $film->FilmOflcRating;
+            //$movie['release_date']      = (string) $film->FilmYear;
+            $movie['duration']          = (string) $film->FilmLength;
+            $movie['country']           = (string) $film->FilmCountry;
+            //$movie['language']          = (string);
+            //$movie['aspect_ratio']      = (string);
+            //$movie['sound_mix']         = (string);
+            //$movie['company']           = (string);
+            //$movie['rating']            = (string);
+            $movie['utf_offset']        = (string) $this->vendor->getUtcOffset();
+
+            //#753 addImageHelper capture Exception and notify, this don't break the Import process
+            $this->addImageHelper( $movie, (string) $film->ImagePath );
+
+            // Save
+            $this->notifyImporter( $movie );
+            
+        } catch ( Exception $ex ) {
+            
+            $this->notifyImporterOfFailure( $ex, isset($movie) ? $movie : null );
+            
+        }
     }
   }
 
--- test/unit/lib/import/sydney/sydneyFtpMoviesMapperTest.php
+++ test/unit/lib/import/sydney/sydneyFtpMoviesMapperTest.php
@@ -2,6 +2,7 @@
 require_once 'PHPUnit/Framework.php';
 require_once dirname( __FILE__ ) . '/../../../../../test/bootstrap/unit.php';
 require_once dirname( __FILE__ ) . '/../../../bootstrap.php';
+require_once TO_TEST_MOCKS . '/FTPClient.mock.php';
 
 /**
  * Test class for sydney venues import
@@ -10,29 +11,36 @@ require_once dirname( __FILE__ ) . '/../../../bootstrap.php';
  * @subpackage sydney.import.lib.unit
  *
  * @author Peter Johnson <peterjohnson@timeout.com>
+ * @author Rajeevan Kumarathasan <rajeevankumarathasan.com>
  *
  * @version 1.0.1
  */
 class sydneyFtpMoviesMapperTest extends PHPUnit_Framework_TestCase
 {
+    private $vendor;
+    private $params;
+  
   public function setUp()
   {
     ProjectN_Test_Unit_Factory::createDatabases();
+    Doctrine::loadData('data/fixtures');
 
-    $feed     = simplexml_load_file( TO_TEST_DATA_PATH . '/sydney_sample_films.xml' );
-    $vendor   =  ProjectN_Test_Unit_Factory::add( 'Vendor',  array(
-                                                     'city'          => 'sydney',
-                                                     'language'      => 'en-AU',
-                                                     'country_code'  => 'au',
-                                                     'country_code_long'  => 'AUS',
-                                                     'inernational_dial_code'  => '+61',
-                                                     ) );
+    $this->vendor = Doctrine::getTable( 'Vendor' )->findOneByCity('sydney');
+    $this->params = array( 'type' => 'movie', 'ftp' => array(
+                                                        'classname' => 'FTPClientMock',
+                                                        'username' => 'test',
+                                                        'password' => 'test',
+                                                        'src' => '',
+                                                        'dir' => '/',
+                                                        'file' => TO_TEST_DATA_PATH . '/sydney/sydney_sample_films.xml'
+                                                        )
+        );
 
+    // Run Import
     $importer = new Importer();
-    $importer->addDataMapper( new sydneyFtpMoviesMapper( $vendor, $feed ) );
+    $importer->addDataMapper( new sydneyFtpMoviesMapper( $this->vendor, $this->params ) );
     $importer->run();
-
-
+        
   }
 
   public function tearDown()
