490926d92e8f3fd50651826a065fdc3091c17906
Rajeevan <rajeevankumarathasan@timeout.com>
2010-11-23 15:45:28 +0000

    refs #807 - Test deleted, since we don't check header of Image added anymore; this test is invalid and cannot be tested in this model'

--- test/unit/lib/model/doctrine/PoiTest.php
+++ test/unit/lib/model/doctrine/PoiTest.php
@@ -574,21 +574,6 @@ class PoiTest extends PHPUnit_Framework_TestCase
       $this->assertEquals( '+3493 9 3424 6577', $poi['phone'], "formatPhone should'nt change the phone number if it's already prefixed with the dial code" );
    }
 
-   /**
-    * Check addMediaByUrl() get_header for array value.
-    */
-   public function testAddMediaByUrlMimeTypeCheck()
-   {
-      $poi = ProjectN_Test_Unit_Factory::get( 'Poi' );
-
-      // Valid URL with 302 Redirect
-      $this->assertTrue( $poi->addMediaByUrl( 'http://www.timeout.com/img/44494/image.jpg' ), 'addMediaByUrl() should return true if header check is valid ' );
-      // 404 Error Url
-      $this->assertFalse( $poi->addMediaByUrl( 'http://www.toimg.net/managed/images/a10038317/image.jpg' ), 'This should fail as This is invalid URL ' );
-      // Valid URL - No redirect
-      $this->assertTrue( $poi->addMediaByUrl( 'http://www.toimg.net/managed/images/10038317/image.jpg' ), 'This should fail as This is invalid URL ' );
-   }
-
    public function testAddVendorCategoryHTMLDecode()
    {
     $vendorCategory = "Neighborhood &amp; pubs";
