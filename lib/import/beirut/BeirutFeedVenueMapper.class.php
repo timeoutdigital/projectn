b61302c947b65ede2565d1a69c8d5613bb360610
Rajeevan <rajeevankumarathasan@timeout.com>
2010-12-03 17:05:31 +0000

    refs #830 - wrong column names corrected and ->save() changed to notifier for logging...

--- lib/import/beirut/BeirutFeedVenueMapper.class.php
+++ lib/import/beirut/BeirutFeedVenueMapper.class.php
@@ -29,29 +29,28 @@ class BeirutFeedVenueMapper extends BeirutFeedBaseMapper
                 }
 
                 // Map data
-                $poi['Vendor'] = $this->vendor;
-                $poi['vendor_poi_id'] = $vendorPoiID;
+                $poi['Vendor']                      = $this->vendor;
+                $poi['vendor_poi_id']               = $vendorPoiID;
 
-                $poi['name'] = $this->clean( (string)$xmlNode->name );
+                $poi['poi_name']                    = $this->clean( (string)$xmlNode->name );
 
-                $poi['house_no'] = $this->clean( (string)$xmlNode->house_no );
-                $poi['street'] = $this->clean( (string)$xmlNode->street );
-                $poi['city'] = "Beirut";
-                $poi['district'] = $this->clean( (string)$xmlNode->district );
-                $poi['country'] = $this->vendor['country_code_long'];
-                $poi['postcode'] = $this->clean( (string)$xmlNode->postcode );
-                $poi['additional_address_details'] = $this->clean( (string)$xmlNode->additional_address_details );
-                $poi['public_transport'] = $this->clean( (string)$xmlNode->public_transport );
+                $poi['house_no']                    = $this->clean( (string)$xmlNode->house_no );
+                $poi['street']                      = $this->clean( (string)$xmlNode->street );
+                $poi['city']                        = "Beirut";
+                $poi['district']                    = $this->clean( (string)$xmlNode->district );
+                $poi['country']                     = $this->vendor['country_code_long'];
+                $poi['zips']                        = $this->clean( (string)$xmlNode->postcode );
+                $poi['additional_address_details']  = $this->clean( (string)$xmlNode->additional_address_details );
+                $poi['public_transport_links']      = $this->clean( (string)$xmlNode->public_transport );
                 
-                $poi['opening_times'] = $this->clean( (string)$xmlNode->poopening_timestcode );
-                $poi['email'] = $this->clean( (string)$xmlNode->email );
-                $poi['phone'] = $this->clean( (string)$xmlNode->phone );
-                $poi['phone2'] = $this->clean( (string)$xmlNode->phone2 );
-                $poi['fax'] = $this->clean( (string)$xmlNode->fax );
-                $poi['url'] = $this->clean( (string)$xmlNode->url );
-                $poi['provider'] = $this->clean( (string)$xmlNode->provider );
-
-                $poi['rating'] = $this->roundNumberOrNull( $this->clean( (string)$xmlNode->rating ) );
+                $poi['openingtimes']                = $this->clean( (string)$xmlNode->poopening_timestcode );
+                $poi['email']                       = $this->clean( (string)$xmlNode->email );
+                $poi['phone']                       = $this->clean( (string)$xmlNode->phone );
+                $poi['phone2']                      = $this->clean( (string)$xmlNode->phone2 );
+                $poi['fax']                         = $this->clean( (string)$xmlNode->fax );
+                $poi['url']                         = $this->clean( (string)$xmlNode->url );
+                $poi['provider']                    = $this->clean( (string)$xmlNode->provider );
+                $poi['rating']                      = $this->roundNumberOrNull( $this->clean( (string)$xmlNode->rating ) );
                 
 
                 /**
@@ -70,11 +69,13 @@ class BeirutFeedVenueMapper extends BeirutFeedBaseMapper
                 
                 $this->addVendorCategory( $poi, $xmlNode );
 
-                $poi->save();
-                // $this->notifyImporter( $poi );
+                $this->notifyImporter( $poi );
 
             } catch (Exception $e ) {
+
+                echo 'EXCEPTION:' . $e->getMessage() . PHP_EOL;
                 $this->notifyImporterOfFailure( $e, isset( $poi ) ? $poi : null );
+                
             }
         }
     }
