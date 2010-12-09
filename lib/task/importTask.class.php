1f7cba2b807cf1b648de2d87314599a13689ca46
Ralph Schwaninger <ralphschwaninger@timeout.com>
2010-12-09 15:19:17 +0000

    refs #826 change concat call due to method param change

--- lib/task/importTask.class.php
+++ lib/task/importTask.class.php
@@ -215,7 +215,7 @@ class importTask extends sfBaseTask
 
             echo "Running Lisbon Mappers" . PHP_EOL;
 
-            $concatenatedFeed = XmlConcatenator::concatXML( $eventDataSimpleXMLSegmentsArray, '/geral/listings' );
+            $concatenatedFeed = XmlConcatenator::concatXML( $eventDataSimpleXMLSegmentsArray, 'geral' );
             
             $importer->addDataMapper( new LisbonFeedListingsMapper( $concatenatedFeed ) );
 
