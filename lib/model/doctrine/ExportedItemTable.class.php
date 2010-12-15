6205fad5041f4256fb0e767812a62113cbd3b1ef
Rajeevan <rajeevankumarathasan@timeout.com>
2010-12-15 17:37:36 +0000

    refs #860 - Model table updated with function to get value category and add record/update record

--- lib/model/doctrine/ExportedItemTable.class.php
+++ lib/model/doctrine/ExportedItemTable.class.php
@@ -2,7 +2,8 @@
 
 class ExportedItemTable extends Doctrine_Table
 {
-    public function saveRecord( $xmlNode, $modelType )
+    
+    public function saveRecord( $xmlNode, $modelType, $vendorID )
     {
         // Pre-process
         $modelType = strtolower( $modelType );
@@ -12,27 +13,91 @@ class ExportedItemTable extends Doctrine_Table
         }
         
         // Get ID from xmlNode, Poi have attribue "vpid" for id and Event & Move had attribue "id" for their unique ID
-        $recordID = ( $modelType == 'poi' ) ? $xmlNode['vpid'] : $xmlNode['id'];
+        $recordID = ( $modelType == 'poi' ) ? (string)$xmlNode['vpid'] : (string)$xmlNode['id'];
+        $recordID = intval( substr( $recordID , 3 ) ); // strip Airport code and 0's at front
 
-        $record = $this->findOneByModelAndRecordId( $modelType, $recordID );
-
-        if( $record === false ) // add this as new Record
+        // Get UI category ID, No UI category = 0 ID
+        $ui_category_id = $this->getHighestValueUICategoryID( $xmlNode );
+        if( $ui_category_id == null )
         {
-            $record = new ExportedItemTable;
-            $record['record_id'] = $recordID;
-            $record['model'] = $modelType;
-            
+            $ui_category_id = 0; // When no UI category found,set id as 0
         }
-        else // Update any Modification changes
+
+        try
         {
-            
+            // Find existing or create new
+            $record = $this->findOneByModelAndRecordId( $modelType, $recordID );
+
+            if( $record === false ) // add this as new Record
+            {
+                $record = new ExportedItem;
+                $record['record_id'] = $recordID;
+                $record['model'] = $modelType;
+                $record['ui_category_id'] = $ui_category_id;
+                $record['vendor_id'] = $vendorID;
+            }
+            else // Update any Modification changes
+            {
+                if( $record['ui_category_id'] != $ui_category_id )
+                {
+                    $exportedItemModification = new ExportedItemModification;
+                    $exportedItemModification['field'] = 'ui_category_id';
+                    $exportedItemModification['value_before_change'] = $record['ui_category_id'];
+
+                    $record['ExportedItemModification'][] = $exportedItemModification;
+                    $record['ui_category_id'] = $ui_category_id;
+                }
+            }
+
+            $record->save();
+
+        } catch ( Exception $e ) {
+            // @todo: process Exception
+            throw $e;
         }
     }
 
-    private function _getUICategoryID( $xmlNode )
+    /**
+     * Pick UI Category with highest business value.
+     * @param SimpleXMLElement $xmlNode
+     * @return string of highest UI Category or false on failure.
+     */
+    private function getHighestValueUICategoryID( $xmlNode )
     {
-        $propertyUICategory = $xmlNode->xpath( '//property[key="UI_CATEGORY"]/text()' );
+        // Extract UI category from xmlNode and Get only the Unique Category
+        $propertyUICategory = array_unique( $xmlNode->xpath( '//property[@key = "UI_CATEGORY"]' ) );
+
+        if( empty( $propertyUICategory ) )
+        {
+            return null;
+        }
+        
+        // For each Unique UI category, Find the Best (Money value) category for this Record
+        $highestCategory = 99999;
+        $priority = array( 'Eating & Drinking', 'Film', 'Art', 'Around Town', 'Nightlife', 'Music', 'Stage' );
+        
+        foreach( $propertyUICategory as $category )
+        {
+            $uiCatName = (string)$category;
+            
+            $priorityValue = array_search( $uiCatName, $priority );
+            if( is_numeric( $priorityValue ) && $priorityValue < $highestCategory )
+            {
+                $highestCategory = $priorityValue;
+            }
+
+            if( $highestCategory === 0 )
+            {
+                break;
+            }
+        }
+
+        $categoryName = ( array_key_exists( $highestCategory, $priority ) ) ? $priority[ $priorityValue ] : null;
+        $uiCategory = Doctrine::getTable( 'UiCategory' )->findOneByName( $categoryName );
+        
+        return ( $uiCategory === false ) ? null : $uiCategory['id'];
     }
+    
 }
 
 class ExportedItemTableException extends Exception {}
