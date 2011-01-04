9a371eaab978192017638253cbd731fb5b99574d
Rajeevan <rajeevankumarathasan@timeout.com>
2011-01-04 15:52:09 +0000

    refs #863 - Added Filter by option to poi List page

--- apps/backend/modules/poi/lib/filter/doctrine/PoiBackendFormFilter.class.php
+++ apps/backend/modules/poi/lib/filter/doctrine/PoiBackendFormFilter.class.php
@@ -10,7 +10,71 @@ class PoiBackendFormFilter extends BasePoiFormFilter
     {
         $this->setWidget('id', new sfWidgetFormFilterInput(array('with_empty' => false)));
         $this->setValidator('id', new sfValidatorSchemaFilter('text', new sfValidatorNumber(array('required' => false))));
+
+        // add Filter widget
+        $this->setWidget( 'filter_by', new sfWidgetFormChoice(
+                                                            array( 'choices' =>
+                                                                    array(
+                                                                    'all' => 'Any Poi',
+                                                                    'non-duplicate' => 'Non-Duplicate Poi',
+                                                                    'master' => 'Master Poi',
+                                                                    'duplicate' => 'Duplicate Poi'
+                                                                    )
+                                                                ),
+                                                            array(
+                                                                'onchange' => 'this.form.submit();'
+                                                            )
+                                                              )
+                        );
+        
+        $this->setValidator( 'filter_by', new sfValidatorString(array('required' => false)) );
+        
+        parent::configure();
     }
 
+    /**
+     * getFields() added this dynamic custom field "reference" since it don't exists,
+     * we require to override here and manually set the query string
+     * @param <type> $query
+     * @param <type> $field
+     * @param <type> $value
+     */
+    public function addFilterByColumnQuery( Doctrine_Query $query, $field, $value )
+    {
+        $poi = $query->getRootAlias();
+        
+        switch( strtolower( $value ) )
+        {
+            case 'master':
+                
+                $query->innerJoin( "$poi.PoiReference pr ON pr.master_poi_id = $poi.id " );
+                
+                break;
+            case 'duplicate':
+                
+                $query->innerJoin( "$poi.PoiReference pr ON pr.duplicate_poi_id = $poi.id " );
+
+                break;
+            case 'non-duplicate':
+
+                $query->leftJoin( "$poi.PoiReference pr ON pr.duplicate_poi_id = $poi.id " );
+                $query->andWhere( 'pr.master_poi_id IS NULL' );
+
+                break;
+        }
+        return $query;
+    }
+
+    /**
+     * This function will help adding dynamic column that can be filtered
+     * @return array
+     */
+    public function getFields()
+    {
+        // Add a Custom dynamic field call "reference"
+        $fields = parent::getFields();
+        $fields['filter_by'] = 'custom';
+        return $fields;
+    }
 } 
 ?>
