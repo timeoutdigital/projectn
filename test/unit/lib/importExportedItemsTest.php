03ef4cdd05314d5ecdbaae376ae5cd9227e77898
Rajeevan <rajeevankumarathasan@timeout.com>
2011-01-07 17:50:06 +0000

    refs #868 - Test updated to check for imports..

--- test/unit/lib/importExportedItemsTest.php
+++ test/unit/lib/importExportedItemsTest.php
@@ -37,7 +37,7 @@ class importExportedItemsTest extends PHPUnit_Framework_TestCase
         ProjectN_Test_Unit_Factory::destroyDatabases();
     }
 
-    public function testImportExportedItems()
+    public function testImportExportedItemsFirstDay()
     {
         $this->assertEquals( 0, Doctrine::getTable( 'ExportedItem' )->count() );
         $this->assertEquals( 0, Doctrine::getTable( 'ExportedItemHistory' )->count() );
@@ -46,6 +46,44 @@ class importExportedItemsTest extends PHPUnit_Framework_TestCase
 
         $importExported = new importExportedItems( $export_date_path );
         $importExported->import();
-        
+
+        // Check import count to ensure that all imported
+        $this->assertEquals( 18, Doctrine::getTable( 'ExportedItem' )->count() );
+        $this->assertEquals( 18, Doctrine::getTable( 'ExportedItemHistory' )->count() );
+    }
+
+    public function testImportExportedItemsSecondDay()
+    {
+        $this->assertEquals( 0, Doctrine::getTable( 'ExportedItem' )->count() );
+        $this->assertEquals( 0, Doctrine::getTable( 'ExportedItemHistory' )->count() );
+
+        $export_date_path = TO_TEST_DATA_PATH . '/import_exported/export_20110106';
+
+        $importExported = new importExportedItems( $export_date_path );
+        $importExported->import();
+
+        // Check import count to ensure that all imported
+        $this->assertEquals( 25, Doctrine::getTable( 'ExportedItem' )->count() );
+        $this->assertEquals( 25, Doctrine::getTable( 'ExportedItemHistory' )->count() );
+    }
+
+    public function testImportExportedItemsHistory()
+    {
+        $this->assertEquals( 0, Doctrine::getTable( 'ExportedItem' )->count() );
+        $this->assertEquals( 0, Doctrine::getTable( 'ExportedItemHistory' )->count() );
+
+        $export_date_path_1 = TO_TEST_DATA_PATH . '/import_exported/export_20110105';
+        $export_date_path_2 = TO_TEST_DATA_PATH . '/import_exported/export_20110106';
+
+        // Import Both days to simulate history
+        $importExported = new importExportedItems( $export_date_path_1 );
+        $importExported->import();
+
+        $importExported = new importExportedItems( $export_date_path_2 );
+        $importExported->import();
+
+        // Check for imported differences
+        $this->assertEquals( 27, Doctrine::getTable( 'ExportedItem' )->count(), 'Should be 27 as There is only 9 New Items are added in second day. original 18 + 9 = 27');
+        $this->assertEquals( 30, Doctrine::getTable( 'ExportedItemHistory' )->count(), 'Should be 30; original 18 + 9 New items + 3 Changing Category records = 30');
     }
 }
