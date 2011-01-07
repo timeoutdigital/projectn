70e035083da8822819aa035590375fe590d9b59f
Rajeevan <rajeevankumarathasan@timeout.com>
2011-01-07 18:37:11 +0000

    refs #868 Skeleton for Task

---
+++ lib/task/importExportedItemsTask.class.php
@@ -0,0 +1,45 @@
+<?php
+
+class importExportedItemsTask extends sfBaseTask
+{
+
+    protected function configure()
+    {
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name','backend'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'project_n'),
+            new sfCommandOption('exportdir', null, sfCommandOption::PARAMETER_REQUIRED, 'Give Full path for Export folder(where export_yyyymmdd folder resides)', null),
+            new sfCommandOption('date', null, sfCommandOption::PARAMETER_OPTIONAL, 'Specify export date (yyyymmdd) to limit to specific date',null),
+            new sfCommandOption('model', null, sfCommandOption::PARAMETER_REQUIRED, 'Model (poi, event, movie) to Limit to specific model only',null),
+
+        ));
+
+        $this->namespace        = 'projectn';
+        $this->name             = 'importExportedItems';
+        $this->briefDescription = '';
+        $this->detailedDescription =  '';
+    }
+
+    protected function execute($arguments = array(), $options = array())
+    {
+        if( !is_string($options['exportdir']) )
+        {
+            throw new Exception( "Invalid Export Dir, use --exportdir='full/path/to/export'" );
+        }
+
+        $allowedOptions = array( 'poi', 'event', 'movie' );
+        if( $options[ 'model' ] != null && !in_array( strtolower( $options[ 'model' ] ), $allowedOptions ) )
+        {
+            throw new Exception( 'Invalid model given! use --model=' . implode('/',$allowedOptions) );
+        }
+
+        // Extabilish database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'] ? $options['connection'] : null)->getConnection();
+
+        
+
+    }
+
+}
