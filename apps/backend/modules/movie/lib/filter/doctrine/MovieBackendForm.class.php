7c4fbc969da6fe4231acdcc4a1df0068df0273c5
Rajeevan <rajeevankumarathasan@timeout.com>
2011-01-13 16:35:45 +0000

    refs #867 - Movies years starting from 1900 to current year + 5

--- apps/backend/modules/movie/lib/filter/doctrine/MovieBackendForm.class.php
+++ apps/backend/modules/movie/lib/filter/doctrine/MovieBackendForm.class.php
@@ -17,7 +17,7 @@ class MovieBackendForm extends MovieForm
     public function  configure() {
         parent::configure();
 
-        $years = range(1995, (date("Y") + 5 ) );
+        $years = range(1900, (date("Y") + 5 ) );
         $this->widgetSchema['release_date'] = new sfWidgetFormDate( array( 'years' => array_combine($years, $years), 'format' => '%day%/%month%/%year%' ) );
         $this->validatorSchema[ 'release_date' ] = new sfValidatorDate( array( 'required' => false ) );
     }
