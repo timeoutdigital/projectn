cef27f4f9c7c8345f6f49105f8b8c64bcb13542b
Rajeevan <rajeevankumarathasan@timeout.com>
2011-01-31 13:56:58 +0000

    refs #909 - Schema file updated with new table and Model classes created

---
+++ lib/model/doctrine/VendorCategoryBlackListTable.class.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class VendorCategoryBlackListTable extends Doctrine_Table
+{
+    
+    public static function getInstance()
+    {
+        return Doctrine_Core::getTable('VendorCategoryBlackList');
+    }
+}
