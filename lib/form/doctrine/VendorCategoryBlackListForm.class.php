99a81beb072a97ea0d8c29729cf5c9deb5685804
Rajeevan <rajeevankumarathasan@timeout.com>
2011-02-03 13:46:12 +0000

    refs #909 - Form add page will use Filter vendor as default and update Last update last used vendor as Filter

--- lib/form/doctrine/VendorCategoryBlackListForm.class.php
+++ lib/form/doctrine/VendorCategoryBlackListForm.class.php
@@ -12,5 +12,35 @@ class VendorCategoryBlackListForm extends BaseVendorCategoryBlackListForm
 {
   public function configure()
   {
+      // Get vendors AKey=> name and sort A-z
+      $vendors_list = Doctrine::getTable( 'Vendor' )->findAll( 'KeyValue' );
+      asort( $vendors_list );
+
+      // override the vendors with new choices
+      $this->widgetSchema['vendor_id'] = new sfWidgetFormChoice( array( 'choices' =>  $vendors_list ) );
+
+      // Getuser and current Filter for Blacklist categories, this will make produce life much easier as
+      // vendors will be consistance in Filter and Add pages
+      $user = sfContext::getInstance()->getUser();
+      $filters = $user->getAttribute( 'category_black_list.filters', array(), 'admin_module' );
+      
+      if( isset( $filters['vendor_id']) )
+      {
+          var_dump( $filters['vendor_id'] );
+          $this->widgetSchema['vendor_id']->setDefault(intval( $filters['vendor_id'] ) );
+      }
   }
+
+  public function  doSave($con = null) {
+        parent::doSave($con);
+
+        // Update Filter based on Last Used vendor
+        $vendor_id =  $this->getValue( 'vendor_id' );
+        if( is_numeric($vendor_id) && $vendor_id > 0 )
+        {
+            $filters = sfContext::getInstance()->getUser()->getAttribute( 'category_black_list.filters', array(), 'admin_module' );
+            $filters['vendor_id'] = intval( $vendor_id );
+            sfContext::getInstance()->getUser()->setAttribute('category_black_list.filters', $filters, 'admin_module' );
+        }
+    }
 }
