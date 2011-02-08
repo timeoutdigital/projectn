b2138937468d4188d2c30ea35602b61fc736ce88
Rajeevan <rajeevankumarathasan@timeout.com>
2011-02-08 16:14:07 +0000

    refs #900 - Added Unsolvable logic to Event

--- apps/backend/modules/event/lib/filter/doctrine/EventBackendFormFilter.class.php
+++ apps/backend/modules/event/lib/filter/doctrine/EventBackendFormFilter.class.php
@@ -10,7 +10,29 @@ class EventBackendFormFilter extends BaseEventFormFilter
     {
         $this->setWidget('id', new sfWidgetFormFilterInput(array('with_empty' => false)));
         $this->setValidator('id', new sfValidatorSchemaFilter('text', new sfValidatorNumber(array('required' => false))));
+
+        $this->setWidget( 'hide_unsolvable', new sfWidgetFormInputCheckbox( ) );
+        $this->setValidator( 'hide_unsolvable', new sfValidatorPass());
     }
 
+    public function addHideUnsolvableColumnQuery( Doctrine_Query $query, $field, $value )
+    {
+        $event = $query->getRootAlias();
+
+        $query->andWhere( "$event.id NOT IN ( SELECT m.record_id FROM EventMeta m WHERE m.lookup = ?)", 'unsolvable' );
+        return $query;
+    }
+
+    /**
+     * This function will help adding dynamic column that can be filtered
+     * @return array
+     */
+    public function getFields()
+    {
+        // Add a Custom dynamic field call "reference"
+        $fields = parent::getFields();
+        $fields['hide_unsolvable'] = 'custom';
+        return $fields;
+    }
 } 
 ?>
