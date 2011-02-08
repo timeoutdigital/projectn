dd718e2335db3971ada5709180295b082357ab3b
Rajeevan <rajeevankumarathasan@timeout.com>
2011-02-08 16:52:19 +0000

    refs #914 - wrote find duplicate category is in VendorPoiCategoryTable and tst

--- lib/model/doctrine/VendorPoiCategoryTable.class.php
+++ lib/model/doctrine/VendorPoiCategoryTable.class.php
@@ -2,4 +2,13 @@
 
 class VendorPoiCategoryTable extends Doctrine_Table
 {
+    public function findConcatDuplicateCategoryIdBy( $vendor_id, $hydrationMode = Doctrine_Core::HYDRATE_RECORD  )
+    {
+        return $this->createQuery()
+            ->select('GROUP_CONCAT( id ) as dupeIds')
+            ->where('vendor_id = ? ', $vendor_id )
+            ->groupBy('name, vendor_id')
+            ->having('count(*) > 1')
+            ->execute( array(), $hydrationMode );
+    }
 }
