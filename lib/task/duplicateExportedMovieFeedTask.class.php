651c0531ec727020ebb7d90eab1f24a48c942c3f
Rajeevan <rajeevankumarathasan@timeout.com>
2011-02-18 17:28:38 +0000

    refs #926 - Task completed to copy movie to another city and replace the ID tags

--- lib/task/duplicateExportedMovieFeedTask.class.php
+++ lib/task/duplicateExportedMovieFeedTask.class.php
@@ -4,11 +4,7 @@ class duplicateExportedMovieFeedTask extends sfBaseTask
 {
   protected function configure()
   {
-    // // add your own arguments here
-    // $this->addArguments(array(
-    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
-    // ));
-
+      
     $this->addOptions(array(
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
@@ -16,6 +12,7 @@ class duplicateExportedMovieFeedTask extends sfBaseTask
       new sfCommandOption('source', null, sfCommandOption::PARAMETER_REQUIRED, 'source vendor City name (Copy from) --source=moscow', null),
       new sfCommandOption('dest', null, sfCommandOption::PARAMETER_REQUIRED, 'destination vendor City name (Copy to) --dest=omsk', null),
       new sfCommandOption('directory', null, sfCommandOption::PARAMETER_REQUIRED, 'Export directory for movies folder --directory=export_20110218', 'export_' . date( 'Ymd') ),
+      new sfCommandOption('override', null, sfCommandOption::PARAMETER_REQUIRED, 'override existing file in destination? --override=true', false ),
     ));
 
     $this->namespace        = 'projectn';
@@ -65,12 +62,58 @@ EOF;
         }
 
         // vaidate export dir and movies
-        //if(is_dir( ))
-print_r( sfConfig::getAll() );
-        // vaidate Export directory exists
+        $exportMovieDir = sfConfig::get('projectn_export') . '/' . $options['directory'] . '/movie';
+        if( !is_dir( $exportMovieDir ) )
+        {
+            throw new DuplicateExportedMovieFeedTaskException( 'Invalid movie Directory specified "'.$options['directory'].'". Movies directory not found: "'.$exportMovieDir.'"' );
+        }
 
+        $this->copyMovies( $sourceVendor, $destVendor, $exportMovieDir, ( $options['override'] === 'true' ? true : false ) );
+        
+    }
 
-        // add your code here
+    /**
+     * Copy movie from source -> dest and update dest vendor details to movie feed
+     * @param Vendor $sourceVendor
+     * @param Vendor $destVendor
+     * @param string $exportMovieDir
+     */
+    private function copyMovies( Vendor $sourceVendor, Vendor $destVendor, $exportMovieDir, $override )
+    {
+        $sourceFilePath = $exportMovieDir . '/' . strtolower( str_replace( ' ', '_', $sourceVendor['city'] ) ) . '.xml';
+        // validate Source Movie file exists
+        if( !file_exists( $sourceFilePath ) )
+        {
+            throw new DuplicateExportedMovieFeedTaskException( 'Source file not found at ' . $sourceFilePath );
+        }
+
+        // read the file and update the variables
+        $sourceRawData = file_get_contents( $sourceFilePath );
+        $sourceXML = simplexml_load_string( $sourceRawData );   // This will be used to validate No. of nodes and validity once updated
+
+        // replace source vendor AIRPOT CODE int the ID with destination vendor AIRPORT CODE
+        $sourceRawData = str_replace( '<movie id="' . strtoupper( $sourceVendor['airport_code']),  '<movie id="' . strtoupper( $destVendor['airport_code']), $sourceRawData );
+
+        // Validate updates don't compromise XML structural integrity
+        $destXML = simplexml_load_string( $sourceRawData );
+
+        if( count( $destXML ) != count( $sourceXML ) )
+        {
+            throw new DuplicateExportedMovieFeedTaskException( "Source xml node count and Destination xml node count don't match!" );
+        }
+
+        // write as new file
+        $destFilePath = $exportMovieDir . '/' . strtolower( str_replace( ' ', '_', $destVendor['city'] ) ) . '.xml';
+
+        if( !$override && file_exists( $destFilePath ) )
+        {
+            throw new DuplicateExportedMovieFeedTaskException( "There a file exists in destination, if you would like to override; please specify --override=true. Destination file: {$destFilePath}"  );
+        }
+
+        // write file in destination
+        file_put_contents( $destFilePath, $destXML->saveXML() );
+
+        // DONE!
     }
 }
 
