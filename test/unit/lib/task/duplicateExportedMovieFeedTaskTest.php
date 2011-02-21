77d7bb0bc6718da96540d582982b11aef78926d1
Rajeevan <rajeevankumarathasan@timeout.com>
2011-02-21 13:16:51 +0000

    refs #926 - test written for the duplicating Movie

--- test/unit/lib/task/duplicateExportedMovieFeedTaskTest.php
+++ test/unit/lib/task/duplicateExportedMovieFeedTaskTest.php
@@ -26,10 +26,8 @@ class duplicateExportedMovieFeedTaskTest extends PHPUnit_Framework_TestCase
     protected function setUp()
     {
         ProjectN_Test_Unit_Factory::createDatabases();
+        Doctrine::loadData('data/fixtures');
         
-        // Change the projectn_export DIR in sfConfig to test Path
-        sfConfig::set( 'projectn_export', sfConfig::get( 'sf_test_dir' ) . '/export' );
-
         // create the Task
         $this->task = new duplicateExportedMovieFeedTask( new sfEventDispatcher, new sfFormatter );
     }
@@ -41,7 +39,89 @@ class duplicateExportedMovieFeedTaskTest extends PHPUnit_Framework_TestCase
 
     public function testDuplicatingMovies()
     {
-        $options = ' --end=prod --source="istanbul_en" --dest=istanbul --directory="duplicates"';
-        $this->task->runFromCLI( new sfCommandManager, $options );
+        $istanbul_en = Doctrine::getTable( 'Vendor' )->findOneByCity( 'istanbul_en' );
+        $istanbul = Doctrine::getTable( 'Vendor' )->findOneByCity( 'istanbul' );
+
+        // ensure that file are there for _en and not for istanbul
+        if( file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' ) )
+            unlink( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' );
+
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul_en.xml' ));
+        $this->assertEquals( false, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' ));
+
+        // Delete if File exists
+        $this->runTask( array(
+            'source' =>'istanbul_en',
+            'dest' => 'istanbul',
+            'directory' => 'export_duplicates'
+        ) );
+
+        // check for files
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul_en.xml' ));
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' ));
+
+        // Open XML and assert
+        $xml = simplexml_load_file( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' );
+        $this->assertEquals(8, count($xml));
+        $this->assertEquals('Tron: Legacy', (string)$xml->movie[0]->name );
+        $this->assertEquals($istanbul['airport_code'] . '000000000000000000000000018910', (string)$xml->movie[0]['id'], 'Movie IDs Airport code should be updated to Istanbul' );
+
+        // Delete the test file
+        unlink( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' );        
+    }
+
+    public function testDuplicatingMoviesTestOveride()
+    {
+        $istanbul_en = Doctrine::getTable( 'Vendor' )->findOneByCity( 'istanbul_en' );
+        $istanbul = Doctrine::getTable( 'Vendor' )->findOneByCity( 'istanbul' );
+
+        // ensure that file are there for _en and not for istanbul
+        if( file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' ) )
+            unlink( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' );
+
+        // Put empty content, whn runner task executed this fiel should be replaced
+        file_put_contents( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' , '');
+
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul_en.xml' ));
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' ));
+
+        // Delete if File exists
+        $this->runTask( array(
+            'source' =>'istanbul_en',
+            'dest' => 'istanbul',
+            'directory' => 'export_duplicates',
+            'override' => 'true',
+        ) );
+
+        // check for files
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul_en.xml' ));
+        $this->assertEquals( true, file_exists( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' ));
+
+        // Open XML and assert
+        $xml = simplexml_load_file( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' );
+        $this->assertEquals(8, count($xml));
+        $this->assertEquals('Tron: Legacy', (string)$xml->movie[0]->name );
+        $this->assertEquals($istanbul['airport_code'] . '000000000000000000000000018910', (string)$xml->movie[0]['id'], 'Movie IDs Airport code should be updated to Istanbul' );
+
+        // Delete the test file
+        unlink( sfConfig::get( 'app_projectn_export' ) . '/export_duplicates/movie/istanbul.xml' );
+    }
+
+    protected function runTask( $options )
+    {
+        $default = array(
+            'connection' => 'project_n',
+            'env' => 'test',
+        );
+
+        $options = array_merge( $default, $options );
+
+        $args = array();
+        foreach( $options as $k => $v )
+        {
+                $args[] = "--$k=$v";
+        }
+        
+        $this->task->run( array(), $args );
     }
 }
