1e2a941bf124b2197c5024f682ef090e37b72d9c
Rajeevan <rajeevankumarathasan@timeout.com>
2011-02-25 11:58:44 +0000

    refs #935 - Added filter to Show / hide whitelisted pois

--- apps/backend/modules/geo_white_list/lib/filter/doctrine/geoWhiteListFilter.class.php
+++ apps/backend/modules/geo_white_list/lib/filter/doctrine/geoWhiteListFilter.class.php
@@ -14,6 +14,13 @@
 
 class geoWhiteListFilter extends BasePoiFormFilter
 {
+    public function  configure() {
+        parent::configure();
+
+        $this->setWidget( 'showmarked', new sfWidgetFormSelect( array( 'choices' => array('hide' => 'Non-Whitelisted', 'show' => 'Whitelisted') )) );
+        $this->setValidator( 'showmarked', new sfValidatorPass() );
+        
+    }
     public function  doBuildQuery(array $values)
     {
         // Apply default vendor when no vendor applied in filter
@@ -21,9 +28,16 @@ class geoWhiteListFilter extends BasePoiFormFilter
         if( !isset($filters['vendor_id']) || $filters['vendor_id'] == null )
         {
             $filters['vendor_id'] = 1;
+            $values['vendor_id'] = 1;
             sfContext::getInstance()->getUser()->setAttribute( 'geo_white_list.filters', $filters, 'admin_module' );
         }
-        
+
+        // Default, show only the unmarked once...
+        if( !isset( $values['showmarked'] ) || null === $values['showmarked'] || '' === $values['showmarked'] )
+        {
+            $values['showmarked'] = 'hide';
+        }
+
         $query = parent::doBuildQuery($values);
         // apply default filter to only return duplicates
         $duplicatesResults = Doctrine::getTable( 'Poi' )->findAllDuplicateLatLongs( $filters['vendor_id'] );
@@ -35,11 +49,29 @@ class geoWhiteListFilter extends BasePoiFormFilter
 
         $alias = $query->getRootAlias();
         $query->leftJoin( "{$alias}.PoiMeta m WITH m.lookup = 'geocodeWhitelist' " );
-        $query->andWhere( 'm.id IS NULL' );
         $query->andWhere( "{$alias}.latitude is not null" );
         $query->andWhere( "{$alias}.longitude is not null" );
         $query->andWhereIn( "CONCAT( {$alias}.latitude, ', ', {$alias}.longitude )" , $duplicates );
         
+        return $query;
+    }
+
+    /**
+     * Switch between shwoing marked and un-marked pois
+     * @param Doctrine_Query $query
+     * @param string $field
+     * @param string $value
+     * @return Doctrine_Query
+     */
+    public function addShowmarkedColumnQuery(Doctrine_Query $query, $field, $value)
+    {
+        if( $value === 'show' )
+        {
+            $query->andWhere( 'm.id IS NOT NULL' );
+        } else {
+            $query->andWhere( 'm.id IS NULL' );
+        }
+
         return $query;
     }
 
@@ -51,6 +83,7 @@ class geoWhiteListFilter extends BasePoiFormFilter
     public function getFields()
     {
         $fields = parent::getFields();
+        $fields['showmarked'] = 'custom';
         return $fields;
     }
 }
